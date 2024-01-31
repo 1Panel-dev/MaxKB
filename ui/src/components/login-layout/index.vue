4fd78e5b7969f125942af835660c9241aeb0c93a
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2024-01-31 16:38:58 +0800

    fix: 兼容问题

--- ui/src/components/login-layout/index.vue
+++ ui/src/components/login-layout/index.vue
@@ -2,15 +2,8 @@
   <div class="login-warp flex-center">
     <div class="login-container w-full h-full">
       <el-row class="container w-full h-full">
-        <el-col
-          :xs="0"
-          :sm="0"
-          :md="10"
-          :lg="10"
-          :xl="10"
-          class="left-container"
-          v-if="screenWidth && screenWidth >= 990"
-        >
+        <!-- v-if="screenWidth && screenWidth >= 990" -->
+        <el-col :xs="0" :sm="0" :md="10" :lg="10" :xl="10" class="left-container">
           <div class="login-image"></div>
         </el-col>
         <el-col :xs="24" :sm="24" :md="14" :lg="14" :xl="14" class="right-container flex-center">
@@ -21,18 +14,18 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import type { Ref } from 'vue'
+// import { ref, onMounted } from 'vue'
+// import type { Ref } from 'vue'
 defineOptions({ name: 'LoginLayout' })
-const screenWidth: Ref<number | null> = ref(null)
-onMounted(() => {
-  screenWidth.value = document.body.clientWidth
-  window.onresize = () => {
-    return (() => {
-      screenWidth.value = document.body.clientWidth
-    })()
-  }
-})
+// const screenWidth: Ref<number | null> = ref(null)
+// onMounted(() => {
+//   screenWidth.value = document.body.clientWidth
+//   window.onresize = () => {
+//     return (() => {
+//       screenWidth.value = document.body.clientWidth
+//     })()
+//   }
+// })
 </script>
 <style lang="scss" scope>
 .login-warp {
