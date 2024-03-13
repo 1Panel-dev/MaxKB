ba9305c405a0e4d16c6dee1e983789c8a3f35679
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2024-03-13 16:03:43 +0800

    feat: markdown编辑器

--- ui/src/views/paragraph/component/ParagraphForm.vue
+++ ui/src/views/paragraph/component/ParagraphForm.vue
@@ -12,7 +12,7 @@
       <span class="lighter" v-else>{{ form.title || '-' }}</span>
     </el-form-item>
     <el-form-item label="分段内容" prop="content">
-      <el-input
+      <!-- <el-input
         v-if="isEdit"
         v-model="form.content"
         placeholder="请输入分段内容"
@@ -20,16 +20,25 @@
         show-word-limit
         :rows="8"
         type="textarea"
-      >
-      </el-input>
-      <span v-else class="break-all lighter">{{ form.content }}</span>
+      > 
+     </el-input>-->
+      <MdEditor
+        v-if="isEdit"
+        v-model="form.content"
+        placeholder="请输入分段内容"
+        :maxLength="4096"
+        :preview="false"
+        style="height: 300px;"
+      />
+      <MdPreview v-else ref="editorRef" editorId="preview-only" :modelValue="form.content" />
+      <!-- <span v-else class="break-all lighter">{{ form.content }}</span> -->
     </el-form-item>
   </el-form>
 </template>
 <script setup lang="ts">
 import { ref, reactive, onUnmounted, watch } from 'vue'
 import type { FormInstance, FormRules } from 'element-plus'
-
+import { MdEditor, MdPreview } from 'md-editor-v3'
 const props = defineProps({
   data: {
     type: Object,
@@ -44,7 +53,10 @@ const form = ref<any>({
 })
 
 const rules = reactive<FormRules>({
-  content: [{ required: true, message: '请输入分段内容', trigger: 'blur' }]
+  content: [
+    { required: true, message: '请输入分段内容', trigger: 'blur' },
+    { max: 4096, message: '内容最多不超过 4096 个字', trigger: 'blur' }
+  ]
 })
 
 const paragraphFormRef = ref<FormInstance>()
