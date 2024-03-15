35d20714986b7407b36f1126a482ab95a7fd3a13
liqiang-fit2cloud <liqiang@fit2cloud.com>
2024-03-15 10:26:41 +0800

    build: 尝试打包allinone

---
+++ installer/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE "maxkb";
+
+\c "maxkb";
+
+CREATE EXTENSION "vector" VERSION '0.5.1';
---
+++ installer/run-maxkb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start postgress
+docker-entrypoint.sh postgres &
+
+# Wait postgress
+until pg_isready --host=3333; do sleep 1 && echo "waiting for postgres"; done
+
+# Start MaxKB
+python /opt/maxkb/app/main.py start &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
