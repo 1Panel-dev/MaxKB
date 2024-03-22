2d91a0f2bf29e892c4dfee00b5460f829515cea0
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2024-03-22 20:18:59 +0800

    fix: 程序启动将正在下载的模型设置为失败状态

--- apps/common/event/__init__.py
+++ apps/common/event/__init__.py
@@ -6,9 +6,13 @@
     @date：2023/11/10 10:43
     @desc:
 """
+import setting.models
+from setting.models import Model
 from .listener_manage import *
 
 
 def run():
     listener_manage.ListenerManagement().run()
     QuerySet(Document).filter(status=Status.embedding).update(**{'status': Status.error})
+    QuerySet(Model).filter(status=setting.models.Status.DOWNLOAD).update(status=setting.models.Status.ERROR,
+                                                                         meta={'message': "下载程序被中断,请重试"})
