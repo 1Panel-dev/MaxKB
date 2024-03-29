c893cd17ff06b1a3e4f5a228ca5450482d775bd2
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2024-03-29 14:29:15 +0800

    fix: 处理优化问题为空字符串的情况下,使用原始问题进行问答

--- apps/application/chat_pipeline/step/reset_problem_step/impl/base_reset_problem_step.py
+++ apps/application/chat_pipeline/step/reset_problem_step/impl/base_reset_problem_step.py
@@ -31,7 +31,10 @@ class BaseResetProblemStep(IResetProblemStep):
         response = chat_model.invoke(message_list)
         padding_problem = problem_text
         if response.content.__contains__("<data>") and response.content.__contains__('</data>'):
-            padding_problem = response.content[response.content.index('<data>') + 6:response.content.index('</data>')]
+            padding_problem_data = response.content[
+                                   response.content.index('<data>') + 6:response.content.index('</data>')]
+            if padding_problem_data is not None and len(padding_problem_data.strip()) > 0:
+                padding_problem = padding_problem_data
         self.context['message_tokens'] = chat_model.get_num_tokens_from_messages(message_list)
         self.context['answer_tokens'] = chat_model.get_num_tokens(padding_problem)
         return padding_problem
