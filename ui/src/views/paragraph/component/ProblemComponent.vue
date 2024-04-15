c226a49af792d20085e2f2b215ad37d597f08d0c
shaohuzhang1 <80892890+shaohuzhang1@users.noreply.github.com>
2024-04-15 14:18:33 +0800

    Pr@main@fix hit test (#70)
    * fix: 应用命中测试报没有权限访问 #65

--- ui/src/views/paragraph/component/ProblemComponent.vue
+++ ui/src/views/paragraph/component/ProblemComponent.vue
@@ -168,7 +168,7 @@ const remoteMethod = (query: string) => {
 function getProblemOption(filterText?: string) {
   return problem
     .asyncGetProblem(
-      id as string,
+      props.datasetId || (id as string),
       { current_page: 1, page_size: 100 },
       filterText && { content: filterText },
       optionLoading
