f442fcc3d6d496447e035fb5dfc14f2b3f17ac08
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2024-04-18 15:40:07 +0800

    fix: 修复openai模型在对接其他兼容openai接口平台时获取tokens错误

---
+++ apps/setting/models_provider/impl/openai_model_provider/model/openai_chat_model.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+"""
+    @project: maxkb
+    @Author：虎
+    @file： openai_chat_model.py
+    @date：2024/4/18 15:28
+    @desc:
+"""
+from typing import List
+
+from langchain_core.messages import BaseMessage, get_buffer_string
+from langchain_openai import ChatOpenAI
+
+
+class TokenizerManage:
+    tokenizer = None
+
+    @staticmethod
+    def get_tokenizer():
+        from transformers import GPT2TokenizerFast
+        if TokenizerManage.tokenizer is None:
+            TokenizerManage.tokenizer = GPT2TokenizerFast.from_pretrained('gpt2',
+                                                                          cache_dir="/opt/maxkb/model/tokenizer",
+                                                                          resume_download=False,
+                                                                          force_download=False)
+        return TokenizerManage.tokenizer
+
+
+class OpenAIChatModel(ChatOpenAI):
+    def get_num_tokens_from_messages(self, messages: List[BaseMessage]) -> int:
+        try:
+            return super().get_num_tokens_from_messages(messages)
+        except Exception as e:
+            tokenizer = TokenizerManage.get_tokenizer()
+            return sum([len(tokenizer.encode(get_buffer_string([m]))) for m in messages])
+
+    def get_num_tokens(self, text: str) -> int:
+        try:
+            return super().get_num_tokens(text)
+        except Exception as e:
+            tokenizer = TokenizerManage.get_tokenizer()
+            return len(tokenizer.encode(text))
