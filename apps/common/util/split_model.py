1f5cc19d68cda8cc22a59457f7917696ff725113
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2024-06-13 10:37:19 +0800

    fix: 修复导入带有表格样式的md文件分段后表格格式丢失 #615

--- apps/common/util/split_model.py
+++ apps/common/util/split_model.py
@@ -335,6 +335,7 @@ class SplitModel:
         :param text: 文本数据
         :return: 解析后数据 {content:段落数据,keywords:[‘段落关键词’],parent_chain:['段落父级链路']}
         """
+        text = text.replace('\r\n', '\n')
         text = text.replace('\r', '\n')
         text = text.replace("\0", '')
         result_tree = self.parse_to_tree(text, 0)
