7fd4fb08c4829a3b34f730b0084f81e1fd3ca807
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2024-06-24 09:59:17 +0800

    feat: 对话纪要添加详情字段

--- apps/application/serializers/chat_serializers.py
+++ apps/application/serializers/chat_serializers.py
@@ -424,7 +424,8 @@ class ChatRecordSerializer(serializers.Serializer):
                 'padding_problem_text': chat_record.details.get('problem_padding').get(
                     'padding_problem_text') if 'problem_padding' in chat_record.details else None,
                 'dataset_list': dataset_list,
-                'paragraph_list': paragraph_list
+                'paragraph_list': paragraph_list,
+                'details': chat_record.details
             }
 
         def page(self, current_page: int, page_size: int, with_valid=True):
