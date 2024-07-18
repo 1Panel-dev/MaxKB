5bbb65e4c45183cd7e0757550d32d992de7daffa
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2024-07-18 15:18:49 +0800

    feat: 关联知识库增加校验向量模型

--- ui/src/views/application/component/AddDatasetDialog.vue
+++ ui/src/views/application/component/AddDatasetDialog.vue
@@ -4,21 +4,28 @@
     v-model="dialogVisible"
     width="600"
     append-to-body
+    class="addDataset-dialog"
   >
     <template #header="{ titleId, titleClass }">
-      <div class="my-header flex">
+      <div class="flex-between mb-8">
         <h4 :id="titleId" :class="titleClass">
           {{ $t('views.application.applicationForm.dialogues.addDataset') }}
         </h4>
-        <el-button link class="ml-16" @click="refresh">
-          <el-icon class="mr-4"><Refresh /></el-icon
-          >{{ $t('views.application.applicationForm.dialogues.refresh') }}
-        </el-button>
+        <div class="flex align-center">
+          <el-button link class="ml-16" @click="refresh">
+            <el-icon class="mr-4"><Refresh /></el-icon
+            >{{ $t('views.application.applicationForm.dialogues.refresh') }}
+          </el-button>
+          <el-divider direction="vertical" />
+        </div>
       </div>
+      <el-text type="info" class="color-secondary">
+        所选知识库必须使用相同的 Embedding 模型
+      </el-text>
     </template>
     <el-row :gutter="12" v-loading="loading">
-      <el-col :span="12" v-for="(item, index) in data" :key="index" class="mb-16">
-        <CardCheckbox value-field="id" :data="item" v-model="checkList">
+      <el-col :span="12" v-for="(item, index) in filterData" :key="index" class="mb-16">
+        <CardCheckbox value-field="id" :data="item" v-model="checkList" @change="changeHandle">
           <span class="ellipsis">
             {{ item.name }}
           </span>
@@ -26,19 +33,29 @@
       </el-col>
     </el-row>
     <template #footer>
-      <span class="dialog-footer">
-        <el-button @click.prevent="dialogVisible = false">
-          {{ $t('views.application.applicationForm.buttons.cancel') }}
-        </el-button>
-        <el-button type="primary" @click="submitHandle">
-          {{ $t('views.application.applicationForm.buttons.confirm') }}
-        </el-button>
-      </span>
+      <div class="flex-between">
+        <div>
+          <el-text type="info" class="color-secondary" v-if="checkList.length > 0">
+            已选 {{ checkList.length }} 个知识库
+          </el-text>
+          <el-button link type="primary" v-if="checkList.length > 0" @click="clearCheck">
+            清空
+          </el-button>
+        </div>
+        <span>
+          <el-button @click.prevent="dialogVisible = false">
+            {{ $t('views.application.applicationForm.buttons.cancel') }}
+          </el-button>
+          <el-button type="primary" @click="submitHandle">
+            {{ $t('views.application.applicationForm.buttons.confirm') }}
+          </el-button>
+        </span>
+      </div>
     </template>
   </el-dialog>
 </template>
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 const props = defineProps({
   data: {
     type: Array<any>,
@@ -51,12 +68,31 @@ const emit = defineEmits(['addData', 'refresh'])
 
 const dialogVisible = ref<boolean>(false)
 const checkList = ref([])
+const currentEmbedding = ref('')
+
+const filterData = computed(() => {
+  return currentEmbedding.value
+    ? props.data.filter((v) => v.embedding_mode_id === currentEmbedding.value)
+    : props.data
+})
 
 watch(dialogVisible, (bool) => {
   if (!bool) {
     checkList.value = []
   }
 })
+
+function changeHandle() {
+  if (checkList.value.length === 1) {
+    currentEmbedding.value = props.data.filter(
+      (v) => v.id === checkList.value[0]
+    )[0].embedding_mode_id
+  }
+}
+function clearCheck() {
+  checkList.value = []
+  currentEmbedding.value = ''
+}
 
 const open = (checked: any) => {
   checkList.value = checked
@@ -73,4 +109,13 @@ const refresh = () => {
 
 defineExpose({ open })
 </script>
-<style lang="scss" scope></style>
+<style lang="scss" scope>
+.addDataset-dialog {
+  .el-dialog__header.show-close {
+    padding-right: 15px;
+  }
+  .el-dialog__headerbtn {
+    top: 13px;
+  }
+}
+</style>
