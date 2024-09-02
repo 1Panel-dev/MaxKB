5f6015d48007ed815fadb32c73105b313038e643
Qiyuan Jiao <753525025@qq.com>
2024-09-02 16:17:41 +0800

    fix typos

--- apps/smartdoc/urls.py
+++ apps/smartdoc/urls.py
@@ -5,13 +5,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  forms my_app import views
+    1. Add an import: from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  forms other_app.views import Home
+    1. Add an import: from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function_lib: forms django.urls import include, path
+    1. Import the include() function_lib: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import os
