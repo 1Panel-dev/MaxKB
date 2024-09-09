d48b51c3e0ce93d0d1d6bad2f798b531fec340c6
CaptainB <bin@fit2cloud.com>
2024-09-09 19:10:16 +0800

    feat: 支持用户输入变量
    --story=1016155 --user=刘瑞斌 【应用编排】-支持设置用户输入变量 https://www.tapd.cn/57709429/s/1576480

--- ui/src/workflow/nodes/base-node/index.vue
+++ ui/src/workflow/nodes/base-node/index.vue
@@ -196,6 +196,53 @@
         </el-select>
       </el-form-item>
     </el-form>
+    <div class="flex-between">
+      全局变量
+      <el-button link type="primary" @click="openAddDialog()">
+        <el-icon class="mr-4"><Plus /></el-icon> 添加
+      </el-button>
+    </div>
+    <el-table :data="props.nodeModel.properties.input_field_list" class="mb-16">
+        <el-table-column prop="name" label="变量名" />
+        <el-table-column prop="variable" label="变量" />
+        <el-table-column label="输入类型">
+          <template #default="{ row }">
+            <el-tag type="info" class="info-tag" v-if="row.type === 'input'">文本框</el-tag>
+            <el-tag type="info" class="info-tag" v-if="row.type === 'date'">日期</el-tag>
+            <el-tag type="info" class="info-tag" v-if="row.type === 'select'">下拉选项</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="必填">
+          <template #default="{ row }">
+            <div @click.stop>
+              <el-switch size="small" v-model="row.is_required" />
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column prop="source" label="赋值方式">
+          <template #default="{ row }">
+            {{ row.source === 'user_input' ? '用户输入' : '接口传参' }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" align="left" width="80">
+          <template #default="{ row, $index }">
+            <span class="mr-4">
+              <el-tooltip effect="dark" content="修改" placement="top">
+                <el-button type="primary" text @click.stop="openAddDialog(row, $index)">
+                  <el-icon><EditPen /></el-icon>
+                </el-button>
+              </el-tooltip>
+            </span>
+            <el-tooltip effect="dark" content="删除" placement="top">
+              <el-button type="primary" text @click="deleteField($index)">
+                <el-icon>
+                  <Delete />
+                </el-icon>
+              </el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
     <!-- 回复内容弹出层 -->
     <el-dialog v-model="dialogVisible" title="开场白" append-to-body>
       <MdEditor v-model="cloneContent" :preview="false" :toolbars="[]" :footers="[]"></MdEditor>
@@ -206,6 +253,7 @@
       </template>
     </el-dialog>
   </NodeContainer>
+  <FieldFormDialog ref="FieldFormDialogRef" @refresh="refreshFieldList" />
 </template>
 <script setup lang="ts">
 import { app } from '@/main'
@@ -218,6 +266,8 @@ import { relatedObject } from '@/utils/utils'
 import useStore from '@/stores'
 import applicationApi from '@/api/application'
 import type { Provider } from '@/api/type/model'
+import FieldFormDialog from './component/FieldFormDialog.vue'
+import { MsgError } from '@/utils/message'
 const { model } = useStore()
 
 const {
@@ -304,9 +354,47 @@ function getTTSModel() {
     })
 }
 
+const currentIndex = ref(null)
+const FieldFormDialogRef = ref()
+const inputFieldList = ref<any[]>([])
+
+function openAddDialog(data?: any, index?: any) {
+  if (typeof index !== 'undefined') {
+    currentIndex.value = index
+  }
+
+  FieldFormDialogRef.value.open(data)
+}
+
+function deleteField(index: any) {
+  inputFieldList.value.splice(index, 1)
+}
+
+function refreshFieldList(data: any) {
+  for (let i = 0; i < inputFieldList.value.length; i++) {
+    if (inputFieldList.value[i].variable === data.variable && currentIndex.value !== i) {
+      MsgError('变量已存在: ' + data.variable)
+      return
+    }
+  }
+  if (currentIndex.value !== null) {
+    inputFieldList.value.splice(currentIndex.value, 1, data)
+  } else {
+    inputFieldList.value.push(data)
+  }
+  currentIndex.value = null
+  FieldFormDialogRef.value.close()
+}
+
 
 onMounted(() => {
   set(props.nodeModel, 'validate', validate)
+  if (props.nodeModel.properties.input_field_list) {
+    props.nodeModel.properties.input_field_list.forEach((item: any) => {
+      inputFieldList.value.push(item)
+    })
+  }
+  set(props.nodeModel.properties, 'input_field_list', inputFieldList)
   getProvider()
   getTTSModel()
   getSTTModel()
--- ui/src/workflow/nodes/start-node/index.vue
+++ ui/src/workflow/nodes/start-node/index.vue
@@ -13,6 +13,18 @@
         </el-button>
       </el-tooltip>
     </div>
+    <div v-for="(item, index) in inputFieldList" :key="index"
+         class="flex-between border-r-4 p-8-12 mb-8 layout-bg lighter"
+         @mouseenter="showicon = true"
+         @mouseleave="showicon = false"
+    >
+      <span>{{ item.name }} {{ '{' + item.variable + '}' }}</span>
+      <el-tooltip effect="dark" content="复制参数" placement="top" v-if="showicon === true">
+        <el-button link @click="copyClick('{{' + '全局变量.' + item.variable + '}}')" style="padding: 0">
+          <AppIcon iconName="app-copy"></AppIcon>
+        </el-button>
+      </el-tooltip>
+    </div>
   </NodeContainer>
 </template>
 <script setup lang="ts">
@@ -27,8 +39,23 @@ const globeLabel = '{{全局变量.time}}'
 
 const showicon = ref(false)
 
-// onMounted(() => {
-//   set(props.nodeModel, 'validate', validate)
-// })
+const inputFieldList = ref<any[]>([])
+
+onMounted(() => {
+  props.nodeModel.graphModel.nodes
+    .filter((v: any) => v.id === 'base-node')
+    .map((v: any) => {
+      // eslint-disable-next-line vue/no-mutating-props
+      props.nodeModel.properties.config.globalFields = [
+        {
+          label: '当前时间',
+          value: 'time'
+        }, ...v.properties.input_field_list.map((i: any) => {
+          return { label: i.name, value: i.variable }
+        })
+      ]
+      inputFieldList.value = v.properties.input_field_list
+    })
+})
 </script>
 <style lang="scss" scoped></style>
