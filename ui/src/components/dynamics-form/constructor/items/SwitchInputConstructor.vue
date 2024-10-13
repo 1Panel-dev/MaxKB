d79ffd0be24f65bd48c5f02748b2d0033b12b2ad
zhangshaohu <shaohu.zhang@fit2cloud.com>
2024-10-13 20:03:32 +0800

    feat: 新增动态表单收集器

---
+++ ui/src/components/dynamics-form/constructor/items/SwitchInputConstructor.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-form-item
+    label="默认值"
+    :required="formValue.required"
+    prop="default_value"
+    :rules="formValue.required ? [{ required: true, message: '默认值 为必填属性' }] : []"
+  >
+    <el-switch v-model="formValue.default_value" />
+  </el-form-item>
+</template>
+<script setup lang="ts">
+import { computed, onMounted } from 'vue'
+
+const props = defineProps<{
+  modelValue: any
+}>()
+const emit = defineEmits(['update:modelValue'])
+const formValue = computed({
+  set: (item) => {
+    emit('update:modelValue', item)
+  },
+  get: () => {
+    return props.modelValue
+  }
+})
+
+const getData = () => {
+  return {
+    input_type: 'SwitchInput',
+    attrs: {},
+    default_value: formValue.value.default_value
+  }
+}
+defineExpose({ getData })
+onMounted(() => {
+  formValue.value.default_value = false
+})
+</script>
+<style lang="scss"></style>
