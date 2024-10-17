c137cbffe4fdcabd6881e7353798c30c2d0bbfb2
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2024-10-17 17:43:58 +0800

    feat: 支持百度千帆向量模型

---
+++ apps/setting/models_provider/impl/wenxin_model_provider/credential/embedding.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+"""
+    @project: MaxKB
+    @Author：虎
+    @file： embedding.py
+    @date：2024/10/17 15:40
+    @desc:
+"""
+from typing import Dict
+
+from common import forms
+from common.exception.app_exception import AppApiException
+from common.forms import BaseForm
+from setting.models_provider.base_model_provider import BaseModelCredential, ValidCode
+
+
+class QianfanEmbeddingCredential(BaseForm, BaseModelCredential):
+
+    def is_valid(self, model_type: str, model_name, model_credential: Dict[str, object], provider,
+                 raise_exception=False):
+        model_type_list = provider.get_model_type_list()
+        if not any(list(filter(lambda mt: mt.get('value') == model_type, model_type_list))):
+            raise AppApiException(ValidCode.valid_error.value, f'{model_type} 模型类型不支持')
+        self.valid_form(model_credential)
+        try:
+            model = provider.get_model(model_type, model_name, model_credential)
+            model.embed_query('你好')
+        except Exception as e:
+            if isinstance(e, AppApiException):
+                raise e
+            if raise_exception:
+                raise AppApiException(ValidCode.valid_error.value, f'校验失败,请检查参数是否正确: {str(e)}')
+            else:
+                return False
+        return True
+
+    def encryption_dict(self, model: Dict[str, object]):
+        return {**model, 'qianfan_sk': super().encryption(model.get('qianfan_sk', ''))}
+
+    qianfan_ak = forms.PasswordInputField('API Key', required=True)
+
+    qianfan_sk = forms.PasswordInputField("Secret Key", required=True)
