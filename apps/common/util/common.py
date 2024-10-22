4b60411766a3a33ae3841f8d4466777ddd03f182
wxg0103 <727495428@qq.com>
2024-10-22 17:53:08 +0800

    fix: 修复license校验问题

--- apps/common/util/common.py
+++ apps/common/util/common.py
@@ -77,11 +77,13 @@ def valid_license(model=None, count=None, message=None):
     def inner(func):
         def run(*args, **kwargs):
             xpack_cache = DBModelManage.get_model('xpack_cache')
-            if ((not False if xpack_cache is None else xpack_cache.get('XPACK_LICENSE_IS_VALID', False))
-                    and QuerySet(
-                        model).count() >= count):
-                error = message or f'超出限制{count},请联系我们（https://fit2cloud.com/）。'
-                raise AppApiException(400, error)
+            is_license_valid = xpack_cache.get('XPACK_LICENSE_IS_VALID', False) if xpack_cache is not None else False
+            record_count = QuerySet(model).count()
+
+            if not is_license_valid and record_count >= count:
+                error_message = message or f'超出限制{count}, 请联系我们（https://fit2cloud.com/）。'
+                raise AppApiException(400, error_message)
+
             return func(*args, **kwargs)
 
         return run
