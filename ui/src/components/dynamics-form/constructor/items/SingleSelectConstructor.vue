26588a4aa25e65403f86ff03613c33f2adaf8ee5
CaptainB <bin@fit2cloud.com>
2024-11-29 18:48:48 +0800

    refactor: 添加参数时单选、多选、选项卡设置第一个条目
    --bug=1049778 --user=刘瑞斌 【表单收集】- 组件类型选择单选框、多选框、选项卡、单行选项卡后，默认显示一行选项值。 https://www.tapd.cn/57709429/s/1621602

--- ui/src/components/dynamics-form/constructor/items/SingleSelectConstructor.vue
+++ ui/src/components/dynamics-form/constructor/items/SingleSelectConstructor.vue
@@ -108,6 +108,8 @@ defineExpose({ getData, rander })
 onMounted(() => {
   formValue.value.option_list = []
   formValue.value.default_value = ''
+
+  addOption()
 })
 </script>
 <style lang="scss" scoped>
