fd35c8a9370e91d53f54290d05013222998ce7eb
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2024-12-04 11:59:26 +0800

    fix: 修复项目重启后文档状态不正确

--- apps/common/event/__init__.py
+++ apps/common/event/__init__.py
@@ -10,8 +10,14 @@ import setting.models
 from setting.models import Model
 from .listener_manage import *
 
+update_document_status_sql = """
+UPDATE "public"."document" 
+SET status ="replace"("replace"("replace"(status, '1', '3'), '0', '3'), '4', '3')
+"""
+
 
 def run():
     # QuerySet(Document).filter(status__in=[Status.embedding, Status.queue_up]).update(**{'status': Status.error})
     QuerySet(Model).filter(status=setting.models.Status.DOWNLOAD).update(status=setting.models.Status.ERROR,
                                                                          meta={'message': "下载程序被中断,请重试"})
+    update_execute(update_document_status_sql, [])
