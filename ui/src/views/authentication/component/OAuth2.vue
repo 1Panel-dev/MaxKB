70fcb9ca663438b8ddbf3308b2da9b90aafeffa6
wxg0103 <727495428@qq.com>
2024-12-04 09:58:08 +0800

    refactor: 修改OAuth2

--- ui/src/views/authentication/component/OAuth2.vue
+++ ui/src/views/authentication/component/OAuth2.vue
@@ -78,7 +78,7 @@
   </div>
 </template>
 <script setup lang="ts">
-import { reactive, ref, watch, onMounted } from 'vue'
+import { reactive, ref, onMounted } from 'vue'
 import authApi from '@/api/auth-setting'
 import type { FormInstance, FormRules } from 'element-plus'
 import { t } from '@/locales'
