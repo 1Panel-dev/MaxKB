6b4cee14127006cc148f74ec0d31029873b2e05c
shaohuzhang1 <80892890+shaohuzhang1@users.noreply.github.com>
2024-12-04 14:19:37 +0800

    fix: 修复对话使用api调用无法响应数据 (#1755)

--- apps/common/handle/impl/response/system_to_response.py
+++ apps/common/handle/impl/response/system_to_response.py
@@ -21,15 +21,19 @@ class SystemToResponse(BaseToResponse):
         if other_params is None:
             other_params = {}
         return result.success({'chat_id': str(chat_id), 'id': str(chat_record_id), 'operate': True,
-                               'content': content, 'is_end': is_end, **other_params}, response_status=_status,
+                               'content': content, 'is_end': is_end, **other_params,
+                               'completion_tokens': completion_tokens, 'prompt_tokens': prompt_tokens},
+                              response_status=_status,
                               code=_status)
 
-    def to_stream_chunk_response(self, chat_id, chat_record_id, node_id, up_node_id_list, content, is_end, completion_tokens,
+    def to_stream_chunk_response(self, chat_id, chat_record_id, node_id, up_node_id_list, content, is_end,
+                                 completion_tokens,
                                  prompt_tokens, other_params: dict = None):
         if other_params is None:
             other_params = {}
         chunk = json.dumps({'chat_id': str(chat_id), 'chat_record_id': str(chat_record_id), 'operate': True,
-                            'content': content, 'node_id': node_id, 'up_node_id_list': up_node_id_list, 'is_end': is_end,
+                            'content': content, 'node_id': node_id, 'up_node_id_list': up_node_id_list,
+                            'is_end': is_end,
                             'usage': {'completion_tokens': completion_tokens,
                                       'prompt_tokens': prompt_tokens,
                                       'total_tokens': completion_tokens + prompt_tokens},
