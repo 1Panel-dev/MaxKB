4a8caab6f1f99fb8f7fdc329a556cc4dcbc86b6e
CaptainB <bin@fit2cloud.com>
2024-12-19 12:02:24 +0800

    feat: Support Azure image tts stt model

--- apps/setting/models_provider/impl/azure_model_provider/azure_model_provider.py
+++ apps/setting/models_provider/impl/azure_model_provider/azure_model_provider.py
@@ -12,13 +12,25 @@ from common.util.file_util import get_file_content
 from setting.models_provider.base_model_provider import IModelProvider, ModelProvideInfo, ModelInfo, \
     ModelTypeConst, ModelInfoManage
 from setting.models_provider.impl.azure_model_provider.credential.embedding import AzureOpenAIEmbeddingCredential
+from setting.models_provider.impl.azure_model_provider.credential.image import AzureOpenAIImageModelCredential
 from setting.models_provider.impl.azure_model_provider.credential.llm import AzureLLMModelCredential
+from setting.models_provider.impl.azure_model_provider.credential.stt import AzureOpenAISTTModelCredential
+from setting.models_provider.impl.azure_model_provider.credential.tti import AzureOpenAITextToImageModelCredential
+from setting.models_provider.impl.azure_model_provider.credential.tts import AzureOpenAITTSModelCredential
 from setting.models_provider.impl.azure_model_provider.model.azure_chat_model import AzureChatModel
 from setting.models_provider.impl.azure_model_provider.model.embedding import AzureOpenAIEmbeddingModel
+from setting.models_provider.impl.azure_model_provider.model.image import AzureOpenAIImage
+from setting.models_provider.impl.azure_model_provider.model.stt import AzureOpenAISpeechToText
+from setting.models_provider.impl.azure_model_provider.model.tti import AzureOpenAITextToImage
+from setting.models_provider.impl.azure_model_provider.model.tts import AzureOpenAITextToSpeech
 from smartdoc.conf import PROJECT_DIR
 
 base_azure_llm_model_credential = AzureLLMModelCredential()
 base_azure_embedding_model_credential = AzureOpenAIEmbeddingCredential()
+base_azure_image_model_credential = AzureOpenAIImageModelCredential()
+base_azure_tti_model_credential = AzureOpenAITextToImageModelCredential()
+base_azure_tts_model_credential = AzureOpenAITTSModelCredential()
+base_azure_stt_model_credential = AzureOpenAISTTModelCredential()
 
 default_model_info = ModelInfo('Azure OpenAI', '具体的基础模型由部署名决定', ModelTypeConst.LLM,
                                base_azure_llm_model_credential, AzureChatModel, api_version='2024-02-15-preview'
@@ -36,12 +48,47 @@ embedding_model_info = [
               ),
 ]
 
+image_model_info = [
+    ModelInfo('gpt-4o', '', ModelTypeConst.IMAGE,
+              base_azure_image_model_credential, AzureOpenAIImage, api_version='2023-05-15'
+              ),
+    ModelInfo('gpt-4o-mini', '', ModelTypeConst.IMAGE,
+              base_azure_image_model_credential, AzureOpenAIImage, api_version='2023-05-15'
+              ),
+]
+
+tti_model_info = [
+    ModelInfo('dall-e-3', '', ModelTypeConst.TTI,
+              base_azure_tti_model_credential, AzureOpenAITextToImage, api_version='2023-05-15'
+              ),
+]
+
+tts_model_info = [
+    ModelInfo('tts', '', ModelTypeConst.TTS,
+              base_azure_tts_model_credential, AzureOpenAITextToSpeech, api_version='2023-05-15'
+              ),
+]
+
+stt_model_info = [
+    ModelInfo('whisper', '', ModelTypeConst.STT,
+              base_azure_stt_model_credential, AzureOpenAISpeechToText, api_version='2023-05-15'
+              ),
+]
+
 model_info_manage = (
     ModelInfoManage.builder()
     .append_default_model_info(default_model_info)
     .append_model_info(default_model_info)
     .append_model_info_list(embedding_model_info)
     .append_default_model_info(embedding_model_info[0])
+    .append_model_info_list(image_model_info)
+    .append_default_model_info(image_model_info[0])
+    .append_model_info_list(stt_model_info)
+    .append_default_model_info(stt_model_info[0])
+    .append_model_info_list(tts_model_info)
+    .append_default_model_info(tts_model_info[0])
+    .append_model_info_list(tti_model_info)
+    .append_default_model_info(tti_model_info[0])
     .build()
 )
 
---
+++ apps/setting/models_provider/impl/azure_model_provider/credential/tts.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+from typing import Dict
+
+from common import forms
+from common.exception.app_exception import AppApiException
+from common.forms import BaseForm, TooltipLabel
+from setting.models_provider.base_model_provider import BaseModelCredential, ValidCode
+
+class AzureOpenAITTSModelGeneralParams(BaseForm):
+    # alloy, echo, fable, onyx, nova, shimmer
+    voice = forms.SingleSelect(
+        TooltipLabel('Voice', '尝试不同的声音（合金、回声、寓言、缟玛瑙、新星和闪光），找到一种适合您所需的音调和听众的声音。当前的语音针对英语进行了优化。'),
+        required=True, default_value='alloy',
+        text_field='value',
+        value_field='value',
+        option_list=[
+            {'text': 'alloy', 'value': 'alloy'},
+            {'text': 'echo', 'value': 'echo'},
+            {'text': 'fable', 'value': 'fable'},
+            {'text': 'onyx', 'value': 'onyx'},
+            {'text': 'nova', 'value': 'nova'},
+            {'text': 'shimmer', 'value': 'shimmer'},
+        ])
+
+
+class AzureOpenAITTSModelCredential(BaseForm, BaseModelCredential):
+    api_version = forms.TextInputField("API 版本 (api_version)", required=True)
+    api_base = forms.TextInputField('API 域名 (azure_endpoint)', required=True)
+    api_key = forms.PasswordInputField("API Key (api_key)", required=True)
+
+    def is_valid(self, model_type: str, model_name, model_credential: Dict[str, object], provider,
+                 raise_exception=False):
+        model_type_list = provider.get_model_type_list()
+        if not any(list(filter(lambda mt: mt.get('value') == model_type, model_type_list))):
+            raise AppApiException(ValidCode.valid_error.value, f'{model_type} 模型类型不支持')
+
+        for key in ['api_base', 'api_key', 'api_version']:
+            if key not in model_credential:
+                if raise_exception:
+                    raise AppApiException(ValidCode.valid_error.value, f'{key} 字段为必填字段')
+                else:
+                    return False
+        try:
+            model = provider.get_model(model_type, model_name, model_credential)
+            model.check_auth()
+        except Exception as e:
+            if isinstance(e, AppApiException):
+                raise e
+            if raise_exception:
+                raise AppApiException(ValidCode.valid_error.value, f'校验失败,请检查参数是否正确: {str(e)}')
+            else:
+                return False
+        return True
+
+    def encryption_dict(self, model: Dict[str, object]):
+        return {**model, 'api_key': super().encryption(model.get('api_key', ''))}
+
+    def get_model_params_setting_form(self, model_name):
+        return AzureOpenAITTSModelGeneralParams()
