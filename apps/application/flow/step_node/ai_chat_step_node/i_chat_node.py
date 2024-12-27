7f938eecbe8cbbba4d1bb590d91bf35a1fca382b
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2024-12-27 10:50:51 +0800

    fix: Dialogue node context type is not required

--- apps/application/flow/step_node/ai_chat_step_node/i_chat_node.py
+++ apps/application/flow/step_node/ai_chat_step_node/i_chat_node.py
@@ -26,7 +26,8 @@ class ChatNodeSerializer(serializers.Serializer):
 
     model_params_setting = serializers.DictField(required=False, error_messages=ErrMessage.integer("模型参数相关设置"))
 
-    dialogue_type = serializers.CharField(required=True, error_messages=ErrMessage.char("上下文类型"))
+    dialogue_type = serializers.CharField(required=False, allow_blank=True, allow_null=True,
+                                          error_messages=ErrMessage.char("上下文类型"))
 
 
 class IChatNode(INode):
