5280d81c420948f8f59a927ecc7f9c73399d1b82
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2025-02-28 22:40:43 +0800

    perf: optimization codemirror component

--- ui/src/components/codemirror-editor/index.vue
+++ ui/src/components/codemirror-editor/index.vue
@@ -1,16 +1,50 @@
 <template>
-  <Codemirror
-    v-bind="$attrs"
-    ref="cmRef"
-    :extensions="extensions"
-    :style="codemirrorStyle"
-    :tab-size="4"
-    :autofocus="true"
-  />
+  <div class="codemirror-editor w-full">
+    <Codemirror
+      v-model="data"
+      ref="cmRef"
+      :extensions="extensions"
+      :style="codemirrorStyle"
+      :tab-size="4"
+      :autofocus="true"
+      v-bind="$attrs"
+    />
+
+    <div class="codemirror-editor__footer">
+      <el-button text type="info" @click="openCodemirrorDialog" class="magnify">
+        <AppIcon iconName="app-magnify" style="font-size: 16px"></AppIcon>
+      </el-button>
+    </div>
+    <!-- Codemirror 弹出层 -->
+    <el-dialog
+      v-model="dialogVisible"
+      :title="$t('views.functionLib.functionForm.form.param.code')"
+      append-to-body
+      fullscreen
+    >
+      <Codemirror
+        v-model="cloneContent"
+        :extensions="extensions"
+        :style="codemirrorStyle"
+        :tab-size="4"
+        :autofocus="true"
+        style="
+          height: calc(100vh - 160px) !important;
+          border: 1px solid #bbbfc4;
+          border-radius: 4px;
+        "
+      />
+      <template #footer>
+        <div class="dialog-footer mt-24">
+          <el-button type="primary" @click="submitDialog"> {{ $t('common.confirm') }}</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { Codemirror } from 'vue-codemirror'
 import { python } from '@codemirror/lang-python'
 import { oneDark } from '@codemirror/theme-one-dark'
@@ -18,6 +52,19 @@ import { linter, type Diagnostic } from '@codemirror/lint'
 import FunctionApi from '@/api/function-lib'
 
 defineOptions({ name: 'CodemirrorEditor' })
+
+const props = defineProps<{
+  modelValue: any
+}>()
+const emit = defineEmits(['update:modelValue', 'submitDialog'])
+const data = computed({
+  set: (value) => {
+    emit('update:modelValue', value)
+  },
+  get: () => {
+    return props.modelValue
+  }
+})
 
 function getRangeFromLineAndColumn(state: any, line: number, column: number, end_column?: number) {
   const l = state.doc.line(line)
@@ -52,9 +99,39 @@ const regexpLinter = linter(async (view) => {
 })
 const extensions = [python(), regexpLinter, oneDark]
 const codemirrorStyle = {
-  height: '210px!important'
+  height: '210px!important',
+  width: '100%'
 }
 const cmRef = ref<InstanceType<typeof Codemirror>>()
+// 弹出框相关代码
+const dialogVisible = ref<boolean>(false)
+
+const cloneContent = ref<string>('')
+
+watch(dialogVisible, (bool) => {
+  if (!bool) {
+    emit('submitDialog', cloneContent.value)
+  }
+})
+
+const openCodemirrorDialog = () => {
+  cloneContent.value = props.modelValue
+  dialogVisible.value = true
+}
+
+function submitDialog() {
+  emit('submitDialog', cloneContent.value)
+  dialogVisible.value = false
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.codemirror-editor {
+  position: relative;
+  &__footer {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+  }
+}
+</style>
