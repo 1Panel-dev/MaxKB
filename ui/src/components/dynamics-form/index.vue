8e0396bbdaed37d6bc55ddfba2294b13cba91917
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2025-03-04 11:33:23 +0800

    fix: Fixed the default value of not selecting 'select' after switching models

--- ui/src/components/dynamics-form/index.vue
+++ ui/src/components/dynamics-form/index.vue
@@ -179,11 +179,17 @@ const render = (
     const value = formFieldList.value
       .map((item) => {
         if (form_data[item.field] !== undefined) {
-          const v: any = item.option_list?.filter(i => i.value_field === form_data[item.field])
-          if (v?.length > 0) {
-            return { [item.field]: form_data[item.field] };
+          if (item.value_field && item.option_list && item.option_list.length > 0) {
+            const value_field = item.value_field
+            const find = item.option_list?.find((i) => i[value_field] === form_data[item.field])
+            if (find) {
+              return { [item.field]: form_data[item.field] }
+            }
+            if (item.show_default_value === true || item.show_default_value === undefined) {
+              return { [item.field]: item.default_value }
+            }
           } else {
-            return { [item.field]: item.default_value };
+            return { [item.field]: form_data[item.field] }
           }
         }
         if (item.show_default_value === true || item.show_default_value === undefined) {
@@ -192,7 +198,6 @@ const render = (
         return {}
       })
       .reduce((x, y) => ({ ...x, ...y }), {})
-
     formValue.value = _.cloneDeep(value)
   }
 }
