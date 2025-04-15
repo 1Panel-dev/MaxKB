ec5fd9e343ac99f6dd2ebd2d9bf4d0db174f5251
liqiang-fit2cloud <liqiang@fit2cloud.com>
2025-04-15 14:23:32 +0800

    docs: change defaultSlogan.

--- ui/src/locales/lang/zh-Hant/views/system.ts
+++ ui/src/locales/lang/zh-Hant/views/system.ts
@@ -123,7 +123,7 @@ export default {
     websiteSloganPlaceholder: '請輸入歡迎語',
     websiteSloganTip: '產品 Logo 下的歡迎語',
     logoDefaultTip: '默认为 MaxKB 登錄界面，支持自定义设置',
-    defaultSlogan: '歡迎使用 MaxKB 智能知識庫問答系統',
+    defaultSlogan: '歡迎使用 MaxKB 開源 AI 助手',
     defaultTip: '默認為 MaxKB 平台界面，支持自定義設置',
     platformSetting: '平台設置',
     showUserManual: '顯示用戶手冊',
