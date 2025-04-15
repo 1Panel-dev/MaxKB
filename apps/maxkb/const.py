5ffd3423ff26029b2e8ad0684c02657d88e5c002
CaptainB <bin@fit2cloud.com>
2025-04-15 11:32:49 +0800

    feat: load environment variables from .env file and update config loading logic

--- apps/maxkb/const.py
+++ apps/maxkb/const.py
@@ -2,6 +2,8 @@
 #
 import os
 
+from dotenv import load_dotenv
+
 from .conf import ConfigManager
 
 __all__ = ['BASE_DIR', 'PROJECT_DIR', 'VERSION', 'CONFIG']
@@ -9,4 +11,12 @@ __all__ = ['BASE_DIR', 'PROJECT_DIR', 'VERSION', 'CONFIG']
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(BASE_DIR)
 VERSION = '2.0.0'
-CONFIG = ConfigManager.load_user_config(root_path=os.path.abspath('/opt/maxkb/conf'))
+
+# load environment variables from .env file
+load_dotenv()
+# print(os.getenv('MAXKB_CONFIG'))
+if os.getenv('MAXKB_CONFIG') is not None:
+    CONFIG = ConfigManager.load_user_config(root_path=PROJECT_DIR)
+else:
+    CONFIG = ConfigManager.load_user_config(root_path=os.path.abspath('/opt/maxkb/conf'))
+
