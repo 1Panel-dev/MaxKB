342ef6df307c865befd355adadd25612248c1285
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2025-04-17 10:40:07 +0800

    feat: authentication demo

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('user/login', views.LoginView.as_view(), name='login'),
     path('user/profile', views.UserProfileView.as_view(), name="user_profile"),
     path('user/test', views.TestPermissionsUserView.as_view(), name="test"),
-    path('user/<str:workspace_id>', views.TestWorkspacePermissionUserView.as_view(),
+    path('workspace/<str:workspace_id>/user/profile', views.TestWorkspacePermissionUserView.as_view(),
          name="test_workspace_id_permission")
 ]
