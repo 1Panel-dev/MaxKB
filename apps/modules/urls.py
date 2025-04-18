16cafec6aff6b2b68ba2cf1d4457b3435309f27e
CaptainB <bin@fit2cloud.com>
2025-04-18 21:16:31 +0800

    fix: update module and tool URLs to avoid duplicate paths for ModuleTreeView and ToolTreeView

--- apps/modules/urls.py
+++ apps/modules/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "module"
 urlpatterns = [
     path('workspace/<str:workspace_id>/<str:source>/module', views.ModuleView.Create.as_view()),
-    path('workspace/<str:workspace_id>/<str:source>/module/tree', views.ModuleTreeView.as_view()),
+    path('workspace/<str:workspace_id>/<str:source>/module', views.ModuleTreeView.as_view()),
     path('workspace/<str:workspace_id>/<str:source>/module/<str:module_id>', views.ModuleView.Operate.as_view()),
 ]
