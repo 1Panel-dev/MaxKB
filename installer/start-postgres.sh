f89e18578f91a69ec189f98f060243e11950321b
liqiang-fit2cloud <liqiang@fit2cloud.com>
2025-04-18 15:42:59 +0800

    build: init directory.

--- installer/start-postgres.sh
+++ installer/start-postgres.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p /opt/maxkb/data/postgresql
-
+echo "PostgreSQL starting..."
 docker-entrypoint.sh postgres -c max_connections=${POSTGRES_MAX_CONNECTIONS} &
 sleep 10
-/usr/bin/wait-for-it.sh 127.0.0.1:5432 --timeout=120 --strict
+/usr/bin/wait-for-it.sh 127.0.0.1:5432 --timeout=120 --strict -- echo "PostgreSQL started."
--- installer/start-redis.sh
+++ installer/start-redis.sh
@@ -7,7 +7,7 @@ if [ ! -f /opt/maxkb/conf/redis.conf ]; then
   touch /opt/maxkb/conf/redis.conf
   printf "bind 0.0.0.0\nport 6379\nmaxmemory 1G\nmaxmemory-policy allkeys-lru\ndir /opt/maxkb/data/redis\nrequirepass "${REDIS_PASSWORD}"\n" > /opt/maxkb/conf/redis.conf
 fi
-
+echo "Redis starting..."
 redis-server /opt/maxkb/conf/redis.conf &
 sleep 5
-/usr/bin/wait-for-it.sh 127.0.0.1:6379 --timeout=60 --strict
+/usr/bin/wait-for-it.sh 127.0.0.1:6379 --timeout=60 --strict -- echo "Redis started."
