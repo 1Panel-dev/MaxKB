8071d2dcd55d3c9e912492e67504342bad06eeaa
liqiang-fit2cloud <liqiang@fit2cloud.com>
2025-04-21 15:39:29 +0800

    build: update shell files.

--- installer/start-all.sh
+++ installer/start-all.sh
@@ -3,11 +3,17 @@
 set -e
 
 if [ "$MAXKB_DB_HOST" = "127.0.0.1" ]; then
-    /usr/bin/start-postgres.sh
+  echo "PostgreSQL starting..."
+  /usr/bin/start-postgres.sh &
+  sleep 10
+  wait-for-it 127.0.0.1:5432 --timeout=120 --strict -- echo "PostgreSQL started."
 fi
 
 if [ "$MAXKB_REDIS_HOST" = "127.0.0.1" ]; then
-    /usr/bin/start-redis.sh
+  echo "Redis starting..."
+  /usr/bin/start-redis.sh &
+  sleep 5
+  wait-for-it 127.0.0.1:6379 --timeout=60 --strict -- echo "Redis started."
 fi
 
 /usr/bin/start-maxkb.sh
--- installer/start-postgres.sh
+++ installer/start-postgres.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 mkdir -p /opt/maxkb/data/postgresql
-echo "PostgreSQL starting..."
-docker-entrypoint.sh postgres -c max_connections=${POSTGRES_MAX_CONNECTIONS} &
-sleep 10
-wait-for-it 127.0.0.1:5432 --timeout=120 --strict -- echo "PostgreSQL started."
+docker-entrypoint.sh postgres -c max_connections=${POSTGRES_MAX_CONNECTIONS}
