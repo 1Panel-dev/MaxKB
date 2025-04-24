b5b09dc8b40decd15a843c09b54151a04c64f241
wangdan-fit2cloud <79562285+wangdan-fit2cloud@users.noreply.github.com>
2025-04-24 11:24:44 +0800

    fix: Fixed interface parameter transmission not displaying during debugging (#2969)

--- ui/src/components/ai-chat/index.vue
+++ ui/src/components/ai-chat/index.vue
@@ -162,7 +162,10 @@ const initialApiFormData = ref({})
 const isUserInput = computed(
   () =>
     props.applicationDetails.work_flow?.nodes?.filter((v: any) => v.id === 'base-node')[0]
-      .properties.user_input_field_list.length > 0
+      .properties.user_input_field_list.length > 0 ||
+    (props.type === 'debug-ai-chat' &&
+      props.applicationDetails.work_flow?.nodes?.filter((v: any) => v.id === 'base-node')[0]
+        .properties.api_input_field_list.length > 0)
 )
 const showUserInputContent = computed(() => {
   return ((isUserInput.value && firsUserInput.value) || showUserInput.value) && props.type !== 'log'
