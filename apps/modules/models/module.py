57c6c9916e29fb74363ffbc7a8b4012c485681e2
CaptainB <bin@fit2cloud.com>
2025-04-25 15:37:44 +0800

    feat: implement module depth validation and refactor check_depth method

--- apps/modules/models/module.py
+++ apps/modules/models/module.py
@@ -10,4 +10,6 @@ class ModuleCreateRequest(serializers.Serializer):
 
 
 class ModuleEditRequest(serializers.Serializer):
-    name = serializers.CharField(required=True, label=_('module name'))
+    name = serializers.CharField(required=False, allow_null=True, allow_blank=True, label=_('module name'))
+    parent_id = serializers.CharField(required=False, allow_null=True, allow_blank=True, default='root',
+                                      label=_('parent id'))
