a6533c0db79f0e49fbd809fce978f272817d725a
shaohuzhang1 <80892890+shaohuzhang1@users.noreply.github.com>
2025-04-29 14:57:41 +0800

    feat: Support Alibaba Bailian supplier qwen3 model (#3020)

--- apps/setting/models_provider/impl/aliyun_bai_lian_model_provider/credential/llm.py
+++ apps/setting/models_provider/impl/aliyun_bai_lian_model_provider/credential/llm.py
@@ -47,7 +47,11 @@ class BaiLianLLMModelCredential(BaseForm, BaseModelCredential):
                     return False
         try:
             model = provider.get_model(model_type, model_name, model_credential, **model_params)
-            model.invoke([HumanMessage(content=gettext('Hello'))])
+            if model_params.get('stream'):
+                for res in model.stream([HumanMessage(content=gettext('Hello'))]):
+                    pass
+            else:
+                model.invoke([HumanMessage(content=gettext('Hello'))])
         except Exception as e:
             traceback.print_exc()
             if isinstance(e, AppApiException):
