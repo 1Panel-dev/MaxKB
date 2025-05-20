9a42bd23023de2eba4525ae267221a1a68946557
wxg0103 <727495428@qq.com>
2025-05-20 18:49:22 +0800

    refactor: add client_id

--- apps/application/flow/step_node/application_node/impl/base_application_node.py
+++ apps/application/flow/step_node/application_node/impl/base_application_node.py
@@ -179,7 +179,8 @@ class BaseApplicationNode(IApplicationNode):
         current_chat_id = string_to_uuid(chat_id + application_id)
         Chat.objects.get_or_create(id=current_chat_id, defaults={
             'application_id': application_id,
-            'abstract': message[0:1024]
+            'abstract': message[0:1024],
+            'client_id': client_id,
         })
         if app_document_list is None:
             app_document_list = []
