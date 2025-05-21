2ec0d22b14955ca41ecd4e57be065e5496b21128
wxg0103 <727495428@qq.com>
2025-05-21 18:50:49 +0800

    refactor: openai token

--- apps/setting/models_provider/impl/base_chat_open_ai.py
+++ apps/setting/models_provider/impl/base_chat_open_ai.py
@@ -133,7 +133,7 @@ class BaseChatOpenAI(ChatOpenAI):
         )
 
         usage_metadata: Optional[UsageMetadata] = (
-            _create_usage_metadata(token_usage) if token_usage.get("prompt_tokens") else None
+            _create_usage_metadata(token_usage) if token_usage.get("prompt_tokens") else {}
         )
         if len(choices) == 0:
             # logprobs is implicitly None
