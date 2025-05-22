8f7d91798ba1adf6d15ed3374df3d79cde6478f8
shaohuzhang1 <80892890+shaohuzhang1@users.noreply.github.com>
2025-05-22 10:54:25 +0800

    fix: The dropdown data of subsequent nodes in the form cannot be displayed back (#3129)

--- ui/src/workflow/nodes/form-node/index.ts
+++ ui/src/workflow/nodes/form-node/index.ts
@@ -4,6 +4,24 @@ class FormNode extends AppNode {
   constructor(props: any) {
     super(props, FormNodeVue)
   }
+  get_node_field_list() {
+    const result = []
+    const fields = this.props.model.properties?.config?.fields || []
+    let otherFields = []
+    try {
+      otherFields = this.props.model.properties.node_data.form_field_list.map((item: any) => ({
+        label: typeof item.label == 'string' ? item.label : item.label.label,
+        value: item.field
+      }))
+    } catch (e) {}
+    result.push({
+      value: this.props.model.id,
+      label: this.props.model.properties.stepName,
+      type: this.props.model.type,
+      children: [...fields, ...otherFields]
+    })
+    return result
+  }
 }
 export default {
   type: 'form-node',
