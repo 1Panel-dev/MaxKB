b7ac609358ad35a9228c77264ae9fa9639110564
liuchang <liuchang@fit2cloud.com>
2025-06-09 11:10:09 +0800

    fix(documet):upgrade document updatetime when upgrade document_char_length fixed #2942

--- apps/dataset/sql/update_document_char_length.sql
+++ apps/dataset/sql/update_document_char_length.sql
@@ -2,6 +2,7 @@ UPDATE "document"
 SET "char_length" = ( SELECT CASE WHEN
 		"sum" ( "char_length" ( "content" ) ) IS NULL THEN
 			0 ELSE "sum" ( "char_length" ( "content" ) )
-		END FROM paragraph WHERE "document_id" = %s )
+		END FROM paragraph WHERE "document_id" = %s ),
+    "update_time" = CURRENT_TIMESTAMP
 WHERE
 	"id" = %s
