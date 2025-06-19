5960bd0805a6f7b2f85994e561e53c4345417d68
fit2cloudzhao <452988409@qq.com>
2025-06-19 17:51:20 +0800

    fix: Reorganize the model cache

--- apps/setting/models_provider/impl/xinference_model_provider/model/reranker.py
+++ apps/setting/models_provider/impl/xinference_model_provider/model/reranker.py
@@ -23,6 +23,9 @@ class XInferenceReranker(MaxKBBaseModel, BaseDocumentCompressor):
     api_key: Optional[str]
 
     @staticmethod
+    def is_cache_model():
+        return False
+    @staticmethod
     def new_instance(model_type, model_name, model_credential: Dict[str, object], **model_kwargs):
         return XInferenceReranker(server_url=model_credential.get('server_url'), model_uid=model_name,
                                   api_key=model_credential.get('api_key'), top_n=model_kwargs.get('top_n', 3))
@@ -50,5 +53,6 @@ class XInferenceReranker(MaxKBBaseModel, BaseDocumentCompressor):
         client = RESTfulClient(self.server_url, self.api_key)
         model: RESTfulRerankModelHandle = client.get_model(self.model_uid)
         res = model.rerank([document.page_content for document in documents], query, self.top_n, return_documents=True)
+        print(self.top_n)
         return [Document(page_content=d.get('document', {}).get('text'),
                          metadata={'relevance_score': d.get('relevance_score')}) for d in res.get('results', [])]
