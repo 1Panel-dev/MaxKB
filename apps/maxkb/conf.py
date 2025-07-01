d655028f9e645f28174fac3fde37ffc5d5c16202
liqiang-fit2cloud <liqiang@fit2cloud.com>
2025-07-01 10:24:49 +0800

    feat: add MAXKB_ADMIN_PATH env.

--- apps/maxkb/conf.py
+++ apps/maxkb/conf.py
@@ -104,6 +104,9 @@ class Config(dict):
     def get_sandbox_python_package_paths(self):
         return self.get('SANDBOX_PYTHON_PACKAGE_PATHS', '/opt/py3/lib/python3.11/site-packages,/opt/maxkb-app/sandbox/python-packages,/opt/maxkb/python-packages')
 
+    def get_admin_path(self):
+        return self.get('ADMIN_PATH', '/admin')
+
     def get_session_timeout(self):
         return int(self.get('SESSION_TIMEOUT', 28800))
 
