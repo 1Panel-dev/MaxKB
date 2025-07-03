df6adb1cd29854be1151da8dc6d094c24c6b4ac4
liqiang-fit2cloud <liqiang@fit2cloud.com>
2025-07-03 15:15:34 +0800

    build: update redis.conf.

--- installer/start-redis.sh
+++ installer/start-redis.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 mkdir -p /opt/maxkb/data/redis
+mkdir -p /opt/maxkb/logs
 
 if [ ! -f /opt/maxkb/conf/redis.conf ]; then
   mkdir -p /opt/maxkb/conf
   touch /opt/maxkb/conf/redis.conf
-  printf "bind 0.0.0.0\nport 6379\ndatabases 16\nmaxmemory 1G\nmaxmemory-policy allkeys-lru\ndir /opt/maxkb/data/redis\nrequirepass "${REDIS_PASSWORD}"\n" > /opt/maxkb/conf/redis.conf
+  printf "bind 0.0.0.0\nport 6379\ndatabases 16\nmaxmemory 1G\nmaxmemory-policy allkeys-lru\nloglevel warning\nlogfile /opt/maxkb/logs/redis.log\ndir /opt/maxkb/data/redis\nrequirepass "${REDIS_PASSWORD}"\n" > /opt/maxkb/conf/redis.conf
 fi
 
 redis-server /opt/maxkb/conf/redis.conf
