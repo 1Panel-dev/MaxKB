57b12a20bc73aa526591690ac3e8614eb3f26109
wxg0103 <727495428@qq.com>
2025-07-08 16:40:33 +0800

    refactor: simplify parameter handling in model list retrieval

--- ui/src/views/model/index.vue
+++ ui/src/views/model/index.vue
@@ -200,8 +200,7 @@ const openCreateModel = (provider?: Provider, model_type?: string) => {
 }
 
 const list_model = () => {
-  //const params = active_provider.value?.provider ? { provider: active_provider.value.provider } : {}
-  const params = {}
+  const params = active_provider.value?.provider ? { provider: active_provider.value.provider } : {}
   loadSharedApi({ type: 'model', isShared: isShared.value, systemType: apiType.value })
     .getModelList({ ...model_search_form.value, ...params }, list_model_loading)
     .then((ok: any) => {
