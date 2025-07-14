0356dfed1f68b37181d34d71f995c9d0bcd5fed5
zhangzhanwei <zhanwei.zhang@fit2cloud.com>
2025-07-14 17:39:24 +0800

    feat: Operation log

--- apps/application/views/application_access_token.py
+++ apps/application/views/application_access_token.py
@@ -6,17 +6,28 @@
     @date：2025/6/9 17:42
     @desc:
 """
+from django.db.models import QuerySet
 from django.utils.translation import gettext_lazy as _
 from drf_spectacular.utils import extend_schema
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
 from application.api.application_access_token import ApplicationAccessTokenAPI
+from application.models import Application
 from application.serializers.application_access_token import AccessTokenSerializer
 from common import result
 from common.auth import TokenAuth
 from common.auth.authentication import has_permissions
 from common.constants.permission_constants import PermissionConstants, RoleConstants, ViewPermission, CompareConstants
+from common.log.log import log
+
+def get_application_operation_object(application_id):
+    application_model = QuerySet(model=Application).filter(id=application_id).first()
+    if application_model is not None:
+        return {
+            "name": application_model.name
+        }
+    return {}
 
 
 class AccessToken(APIView):
@@ -31,6 +42,9 @@ class AccessToken(APIView):
         request=ApplicationAccessTokenAPI.get_request(),
         tags=[_('Application')]  # type: ignore
     )
+    @log(menu='Application', operate="Modify application access token",
+         get_operation_object= lambda r,k: get_application_operation_object((k.get('application_id')))
+         )
     @has_permissions(PermissionConstants.APPLICATION_OVERVIEW_ACCESS.get_workspace_application_permission(),
                      PermissionConstants.APPLICATION_OVERVIEW_ACCESS.get_workspace_permission_workspace_manage_role(),
                      ViewPermission([RoleConstants.USER.get_workspace_role()],
