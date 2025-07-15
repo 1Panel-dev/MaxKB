df9245a5a4f3e0b3a908dad2c706db856a24be9e
shaohuzhang1 <80892890+shaohuzhang1@users.noreply.github.com>
2025-07-15 18:35:00 +0800

    feat: Access num reset job (#3614)

--- apps/common/job/__init__.py
+++ apps/common/job/__init__.py
@@ -6,12 +6,13 @@
     @date：2024/3/14 11:54
     @desc:
 """
-#from .client_access_num_job import *
 from .clean_chat_job import *
 from .clean_debug_file_job import *
+from .client_access_num_job import *
 
 
 def run():
-    #client_access_num_job.run()
+    # client_access_num_job.run()
     clean_chat_job.run()
     clean_debug_file_job.run()
+    client_access_num_job.run()
