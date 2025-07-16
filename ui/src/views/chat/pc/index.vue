8869cca9dee783b38db234d4f3d9af14a518f998
teukkk <mengyuan.lv@fit2cloud.com>
2025-07-16 11:18:34 +0800

    fix: closeExecutionDetail

--- ui/src/views/chat/pc/index.vue
+++ ui/src/views/chat/pc/index.vue
@@ -368,6 +368,7 @@ function newChat() {
     paginationConfig.value.total = 0
     currentRecordList.value = []
   }
+  closeExecutionDetail()
   currentChatId.value = 'new'
   currentChatName.value = t('chat.createChat')
   if (common.isMobile()) {
@@ -432,6 +433,7 @@ const clickListHandle = (item: any) => {
     currentRecordList.value = []
     currentChatId.value = item.id
     currentChatName.value = item.abstract
+    closeExecutionDetail()
     if (currentChatId.value !== 'new') {
       getChatRecord()
 
