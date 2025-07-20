6f2db6f488cf4f347a6c8b98f2dd5fa81364d305
CaptainB <bin@fit2cloud.com>
2025-07-20 14:16:13 +0800

    feat: Implement daily log rotation with DailyTimedRotatingFileHandler

--- apps/common/utils/logger.py
+++ apps/common/utils/logger.py
@@ -1,3 +1,29 @@
+from datetime import datetime, timedelta
+from logging.handlers import TimedRotatingFileHandler
+import os
 import logging
 
 maxkb_logger = logging.getLogger('max_kb')
+
+
+class DailyTimedRotatingFileHandler(TimedRotatingFileHandler):
+    def rotator(self, source, dest):
+        """ Override the original method to rotate the log file daily."""
+        dest = self._get_rotate_dest_filename(source)
+        if os.path.exists(source) and not os.path.exists(dest):
+            # 存在多个服务进程时, 保证只有一个进程成功 rotate
+            os.rename(source, dest)
+
+    @staticmethod
+    def _get_rotate_dest_filename(source):
+        date_yesterday = (
+            datetime.now() - timedelta(days=1)
+        ).strftime('%Y-%m-%d')
+        path = [
+            os.path.dirname(source),
+            date_yesterday,
+            os.path.basename(source)
+        ]
+        filename = os.path.join(*path)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        return filename
