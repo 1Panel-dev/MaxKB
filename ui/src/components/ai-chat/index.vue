914cb63de29eea49ea133f683a00da53b0490bca
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2025-07-21 16:33:02 +0800

    fix: Missing i18n messages in English(##3673)

--- ui/src/components/ai-chat/index.vue
+++ ui/src/components/ai-chat/index.vue
@@ -171,13 +171,13 @@ const initialApiFormData = ref({})
 const isUserInput = computed(
   () =>
     props.applicationDetails.work_flow?.nodes?.filter((v: any) => v.id === 'base-node')[0]
-      .properties.user_input_field_list.length > 0,
+      ?.properties.user_input_field_list.length > 0,
 )
 
 const userInputTitle = computed(
   () =>
     props.applicationDetails.work_flow?.nodes?.filter((v: any) => v.id === 'base-node')[0]
-      .properties?.user_input_config?.title,
+      ?.properties?.user_input_config?.title,
 )
 const isAPIInput = computed(
   () =>
