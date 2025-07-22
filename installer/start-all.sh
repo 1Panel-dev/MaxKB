cbb5e69ead054dfb53f6cb1d648d587f3e0b5e67
liqiang-fit2cloud <liqiang@fit2cloud.com>
2025-07-22 11:07:42 +0800

    refactor: print more logs in start-all.sh.

--- installer/start-all.sh
+++ installer/start-all.sh
@@ -4,30 +4,34 @@ set -e
 
 if [ -f "/opt/maxkb/PG_VERSION" ]; then
   # 如果是v1版本一键安装的的目录则退出
-  echo "FATAL ERROR: Upgrade from v1 to v2 is not supported."
-  echo "The process will exit."
+  echo -e "\033[1;31mFATAL ERROR: Upgrade from v1 to v2 is not supported.\033[0m"
+  echo -e "\033[1;31mThe process will exit.\033[0m"
   exit 1
 fi
 
 if [ "$MAXKB_DB_HOST" = "127.0.0.1" ]; then
-  echo "PostgreSQL starting..."
+  echo -e "\033[1;32mPostgreSQL starting...\033[0m"
   /usr/bin/start-postgres.sh &
   postgres_pid=$!
-  sleep 10
-  wait-for-it 127.0.0.1:5432 --timeout=120 --strict -- echo "PostgreSQL started."
+  sleep 5
+  wait-for-it 127.0.0.1:5432 --timeout=120 --strict -- echo -e "\033[1;32mPostgreSQL started.\033[0m"
 fi
 
 if [ "$MAXKB_REDIS_HOST" = "127.0.0.1" ]; then
-  echo "Redis starting..."
+  echo -e "\033[1;32mRedis starting...\033[0m"
   /usr/bin/start-redis.sh &
   redis_pid=$!
   sleep 5
-  wait-for-it 127.0.0.1:6379 --timeout=60 --strict -- echo "Redis started."
+  wait-for-it 127.0.0.1:6379 --timeout=60 --strict -- echo -e "\033[1;32mRedis started.\033[0m"
 fi
 
+echo -e "\033[1;32mMaxKB starting...\033[0m"
 /usr/bin/start-maxkb.sh &
 maxkb_pid=$!
+sleep 10
+wait-for-it 127.0.0.1:8080 --timeout=60 --strict -- echo -e "\033[1;32mMaxKB started.\033[0m"
 
 wait -n
+echo -e "\033[1;31mSystem is shutting down.\033[0m"
 kill $postgres_pid $redis_pid $maxkb_pid 2>/dev/null
 wait
