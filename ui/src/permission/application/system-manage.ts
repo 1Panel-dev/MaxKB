fd11a18c2633f3ac54f2ef4e2f75597ccca8f516
zhangzhanwei <zhanwei.zhang@fit2cloud.com>
2025-07-29 14:47:40 +0800

    feat: Resource tool permission

---
+++ ui/src/permission/application/system-manage.ts
@@ -0,0 +1,106 @@
+import {hasPermission} from '@/utils/permission/index'
+import {ComplexPermission} from '@/utils/permission/type'
+import {EditionConst, PermissionConst, RoleConst} from '@/utils/permission/data'
+
+const systemManage = {
+    create: () => false,
+    folderCreate: () => false,
+    edit: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_EDIT
+            ],
+            'OR'
+    ),
+    folderEdit: () => false,
+    export: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_EXPORT
+            ],
+            'OR'
+    ),
+    delete: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_DELETE     
+            ],
+            'OR'
+    ),
+    folderDelete: () => false,
+    overview_embed: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_OVERVIEW_EMBED
+            ],
+            'OR'
+    ),
+    overview_access: () => 
+      hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_OVERVIEW_ACCESS
+            ],
+            'OR'
+    ),
+    overview_display: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_OVERVIEW_DISPLAY
+            ],
+            'OR'
+    ),
+    overview_api_key: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_OVERVIEW_API_KEY
+            ],
+            'OR'
+    ),
+    access_edit: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_ACCESS_EDIT
+            ],
+            'OR'
+    ),
+    application_chat_user_edit: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_CHAT_USER_EDIT
+            ],
+            'OR'
+    ),
+    chat_log_clear: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_CHAT_LOG_CLEAR_POLICY
+            ],
+            'OR'
+    ),
+    chat_log_export: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_CHAT_LOG_EXPORT
+            ],
+            'OR'
+    ),
+    chat_log_add_knowledge: () => 
+        hasPermission(
+            [
+              RoleConst.ADMIN,
+              PermissionConst.RESOURCE_APPLICATION_CHAT_LOG_ADD_KNOWLEDGE
+            ],
+            'OR'
+    ),    
+}
