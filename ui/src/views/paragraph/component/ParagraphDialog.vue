1febd0a9576860835f3907d7e1ac5eb6ec109d80
zhangzhanwei <zhanwei.zhang@fit2cloud.com>
2025-08-05 11:45:42 +0800

    fix: Problem read permission

--- ui/src/views/paragraph/component/ParagraphDialog.vue
+++ ui/src/views/paragraph/component/ParagraphDialog.vue
@@ -37,6 +37,7 @@
       <el-col :span="6" class="border-l" style="width: 300px">
         <!-- 关联问题 -->
         <ProblemComponent
+           v-if="permissionPrecise.problem_read(id)" 
           :paragraphId="paragraphId"
           :docId="document_id"
           :knowledgeId="id"
@@ -56,12 +57,14 @@
   </el-dialog>
 </template>
 <script setup lang="ts">
-import { ref, watch, nextTick } from 'vue'
+import { ref, watch, nextTick, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { cloneDeep, debounce } from 'lodash'
 import ParagraphForm from '@/views/paragraph/component/ParagraphForm.vue'
 import ProblemComponent from '@/views/paragraph/component/ProblemComponent.vue'
 import { loadSharedApi } from '@/utils/dynamics-api/shared-api'
+import permissionMap from '@/permission'
+
 
 const props = defineProps<{
   title: String
@@ -72,6 +75,21 @@ const route = useRoute()
 const {
   params: { id, documentId },
 } = route as any
+
+const apiType = computed(() => {
+  if (route.path.includes('shared')) {
+    return 'systemShare'
+  } else if (route.query.from == 'systemManage') {
+    return 'systemManage'
+  } else {
+    return 'workspace'
+  }
+})
+
+const permissionPrecise = computed(() => {
+  return permissionMap['knowledge'][apiType.value]
+})
+
 
 const emit = defineEmits(['refresh'])
 
