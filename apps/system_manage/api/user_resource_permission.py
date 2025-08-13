2f7b2415d4812c26bf9094127dffc8a95bf21255
zhangzhanwei <zhanwei.zhang@fit2cloud.com>
2025-08-13 10:09:42 +0800

    rafactor: User resource permission read and edit

--- apps/system_manage/api/user_resource_permission.py
+++ apps/system_manage/api/user_resource_permission.py
@@ -12,15 +12,29 @@ from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
 from common.mixins.api_mixin import APIMixin
-from common.result import ResultSerializer, ResultPageSerializer
-from system_manage.serializers.user_resource_permission import UserResourcePermissionResponse, \
-    UpdateUserResourcePermissionRequest, ResourceUserPermissionEditRequest
+from common.result import ResultSerializer, ResultPageSerializer, PageDataResponse
+from system_manage.serializers.user_resource_permission import ResourceUserPermissionEditRequest, UpdateTeamMemberItemPermissionSerializer
 
 
-class APIUserResourcePermissionResponse(ResultSerializer):
+class UserResourcePermissionResponse0(serializers.Serializer):
+    id = serializers.UUIDField(required=True, label="主键id")
+    name = serializers.CharField(required=True, label="资源名称")
+    auth_target_type = serializers.CharField(required=True, label="授权资源")
+    user_id = serializers.UUIDField(required=True, label="用户id")
+    icon = serializers.CharField(required=True, label="资源图标")
+    auth_type = serializers.CharField(required=True, label="授权类型")
+    permission = serializers.ChoiceField(required=False, allow_null=True, allow_blank=True,
+                                         choices=['NOT_AUTH', 'MANAGE', 'VIEW', 'ROLE'],
+                                         label=_('permission'))
+
+class NewAPIUserResourcePermissionResponse(ResultSerializer):
     def get_data(self):
-        return UserResourcePermissionResponse(many=True)
+        return UserResourcePermissionResponse0(many=True)
 
+class NewAPIUserResourcePermissionPageResponse(ResultPageSerializer):
+
+    def get_data(self):
+        return UserResourcePermissionResponse0(many=True)
 
 class UserResourcePermissionAPI(APIMixin):
     @staticmethod
@@ -40,17 +54,61 @@ class UserResourcePermissionAPI(APIMixin):
                 location='path',
                 required=True,
             ),
+            OpenApiParameter(
+                name="name",
+                description="名称",
+                type=OpenApiTypes.STR,
+                location='query',
+                required=False
+            ),
+            OpenApiParameter(
+                name="permission",
+                description="权限",
+                type=OpenApiTypes.STR,
+                location='query',
+                required=False
+            ),
         ]
 
     @staticmethod
     def get_response():
-        return APIUserResourcePermissionResponse
+        return NewAPIUserResourcePermissionResponse
 
 
 class EditUserResourcePermissionAPI(APIMixin):
     @staticmethod
+    def get_parameters():
+        return [
+            OpenApiParameter(
+                name="workspace_id",
+                description="工作空间id",
+                type=OpenApiTypes.STR,
+                location='path',
+                required=True,
+            ),
+            OpenApiParameter(
+                name="user_id",
+                description="用户id",
+                type=OpenApiTypes.STR,
+                location='path',
+                required=True,
+            ),
+            OpenApiParameter(
+                name="resource",
+                description="资源类型",
+                type=OpenApiTypes.STR,
+                location='path',
+                required=True
+            ),
+        ]
+
+    @staticmethod
     def get_request():
-        return UpdateUserResourcePermissionRequest()
+        return UpdateTeamMemberItemPermissionSerializer(many=True)
+
+    @staticmethod
+    def get_response():
+        return NewAPIUserResourcePermissionResponse
 
 
 class ResourceUserPermissionResponse(serializers.Serializer):
@@ -117,10 +175,69 @@ class ResourceUserPermissionAPI(APIMixin):
     def get_response():
         return APIResourceUserPermissionResponse
 
+class UserResourcePermissionPageAPI(APIMixin):
+    @staticmethod
+    def get_parameters():
+        return [
+            OpenApiParameter(
+                name="workspace_id",
+                description="工作空间id",
+                type=OpenApiTypes.STR,
+                location='path',
+                required=True
+            ),
+            OpenApiParameter(
+                name="user_id",
+                description="用户id",
+                type=OpenApiTypes.STR,
+                location='path',
+                required=True
+            ),
+            OpenApiParameter(
+                name="resource",
+                description="资源类型",
+                type=OpenApiTypes.STR,
+                location='path',
+                required=True
+            ),
+            OpenApiParameter(
+                name="current_page",
+                description=_("Current page"),
+                type=OpenApiTypes.INT,
+                location='path',
+                required=True,
+            ),
+            OpenApiParameter(
+                name="page_size",
+                description=_("Page size"),
+                type=OpenApiTypes.INT,
+                location='path',
+                required=True,
+            ),
+            OpenApiParameter(
+                name="name",
+                description="资源名称",
+                type=OpenApiTypes.STR,
+                location='query',
+                required=False
+            ),
+            OpenApiParameter(
+                name="permission",
+                description="权限",
+                type=OpenApiTypes.STR,
+                location='query',
+                required=False
+            ),
+        ]
+
+    @staticmethod
+    def get_response():
+        return NewAPIUserResourcePermissionPageResponse
+
 
 class APIResourceUserPermissionPageResponse(ResultPageSerializer):
     def get_data(self):
-        return ResourceUserPermissionResponse(many=True)
+        return PageDataResponse(ResourceUserPermissionResponse(many=True))
 
 
 class ResourceUserPermissionPageAPI(APIMixin):
