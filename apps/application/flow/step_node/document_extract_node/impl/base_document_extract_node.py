e9102178505060cc4978464699e2d68438713d17
CaptainB <bin@fit2cloud.com>
2025-08-20 11:17:44 +0800

    fix: remove unnecessary tobytes() calls for file byte retrieval
    --bug=1060633 --user=刘瑞斌 【应用】图片理解节点解析图片报错 https://www.tapd.cn/57709429/s/1758154

--- apps/application/flow/step_node/document_extract_node/impl/base_document_extract_node.py
+++ apps/application/flow/step_node/document_extract_node/impl/base_document_extract_node.py
@@ -66,7 +66,7 @@ class BaseDocumentExtractNode(IDocumentExtractNode):
 
         for doc in document:
             file = QuerySet(File).filter(id=doc['file_id']).first()
-            buffer = io.BytesIO(file.get_byte().tobytes())
+            buffer = io.BytesIO(file.get_byte())
             buffer.name = doc['name']  # this is the important line
 
             for split_handle in (parse_table_handle_list + split_handles):
