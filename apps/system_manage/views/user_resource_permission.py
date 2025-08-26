c082dae066258b7bedbd3624369a7a973d0eb59e
zhangzhanwei <zhanwei.zhang@fit2cloud.com>
2025-08-26 13:57:48 +0800

    fix: Query by permission in system resource management
    --bug=1060785 --user=张展玮 【资源授权】-通过权限查询资源无效 https://www.tapd.cn/62980211/s/1761243

--- apps/system_manage/views/user_resource_permission.py
+++ apps/system_manage/views/user_resource_permission.py
@@ -54,7 +54,7 @@ class WorkSpaceUserResourcePermissionView(APIView):
         return result.success(UserResourcePermissionSerializer(
             data={'workspace_id': workspace_id, 'user_id': user_id, 'auth_target_type': resource}
         ).list({'name': request.query_params.get('name'),
-                'permission': request.query_params.getlist('permission')}, request.user))
+                'permission': request.query_params.getlist('permission[]')}, request.user))
 
     @extend_schema(
         methods=['PUT'],
@@ -99,7 +99,7 @@ class WorkSpaceUserResourcePermissionView(APIView):
             return result.success(UserResourcePermissionSerializer(
                 data={'workspace_id': workspace_id, 'user_id': user_id, 'auth_target_type': resource}
                 ).page({'name': request.query_params.get('name'),
-                        'permission': request.query_params.getlist('permission')}, current_page, page_size, request.user))
+                        'permission': request.query_params.getlist('permission[]')}, current_page, page_size, request.user))
 
 
 class WorkspaceResourceUserPermissionView(APIView):
@@ -132,7 +132,7 @@ class WorkspaceResourceUserPermissionView(APIView):
             data={'workspace_id': workspace_id, "target": target, 'auth_target_type': resource,
                   }).list(
             {'username': request.query_params.get("username"), 'nick_name': request.query_params.get("nick_name"),
-             'permission': request.query_params.getlist("permission")
+             'permission': request.query_params.getlist("permission[]")
              }))
 
     @extend_schema(
