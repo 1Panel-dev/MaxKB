e23a12639a9b35a4d65ebaa8ee005339bfe78f1b
shaohuzhang1 <80892890+shaohuzhang1@users.noreply.github.com>
2025-09-09 11:47:37 +0800

    fix: Unable to add session variables after editing (#4008)

--- ui/src/workflow/nodes/base-node/component/ChatFieldDialog.vue
+++ ui/src/workflow/nodes/base-node/component/ChatFieldDialog.vue
@@ -44,7 +44,7 @@
     </el-form>
     <template #footer>
       <span class="dialog-footer">
-        <el-button @click.prevent="dialogVisible = false"> {{ $t('common.cancel') }} </el-button>
+        <el-button @click.prevent="close"> {{ $t('common.cancel') }} </el-button>
         <el-button type="primary" @click="submit(fieldFormRef)" :loading="loading">
           {{ isEdit ? $t('common.save') : $t('common.add') }}
         </el-button>
