ac30220929b26b4b5c61fa8df5044829d12ac1b9
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2025-12-10 16:53:27 +0800

    fix: When voice playback is turned on, if the answer contains a link, there will be the following abnormal message

--- ui/src/components/ai-chat/component/operation-button/ChatOperationButton.vue
+++ ui/src/components/ai-chat/component/operation-button/ChatOperationButton.vue
@@ -178,6 +178,8 @@ function markdownToPlainText(md: string) {
       .replace(/`(.*?)`/g, '$1')
       // 移除代码块 ```code```
       .replace(/```[\s\S]*?```/g, '')
+      // 移除html标签
+      .replace(/<[^>]+>/g, '')
       // 移除多余的换行符
       .replace(/\n{2,}/g, '\n')
       .trim()
