f18658c9814d8610ab303d06589edea1ad8028db
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2026-01-29 14:20:22 +0800

    fix: Trigger record list

--- apps/trigger/urls.py
+++ apps/trigger/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('workspace/<str:workspace_id>/trigger/<int:current_page>/<int:page_size>', views.TriggerView.Page.as_view(), name='trigger_page'),
     path('workspace/<str:workspace_id>/<str:source_type>/<str:source_id>/trigger/<str:trigger_id>', views.TaskSourceTriggerView.Operate.as_view(), name='task source trigger operate'),
     path('workspace/<str:workspace_id>/<str:source_type>/<str:source_id>/trigger', views.TaskSourceTriggerView.as_view(), name='task source trigger'),
-    path( 'workspace/<str:workspace_id>/trigger/<str:trigger_id>/task_record/<int:current_page>/<int:page_size>', views.TriggerTaskRecordPageView.as_view(), name='trigger_task_record'),
-    path('workspace/<str:workspace_id>/task', views.TriggerTaskView.as_view(), name='task'),
+    path('workspace/<str:workspace_id>/trigger/<str:trigger_id>/task_record/<int:current_page>/<int:page_size>', views.TriggerTaskRecordPageView.as_view(), name='trigger_task_record'),
+    path('workspace/<str:workspace_id>/trigger/<str:trigger_id>/task', views.TriggerTaskView.as_view(), name='task'),
     path('trigger/v1/webhook/<str:trigger_id>', EventTriggerView.as_view(), name='trigger_webhook'),
-    path( 'workspace/<str:workspace_id>/trigger/<str:trigger_id>/trigger_task/<str:trigger_task_id>/trigger_task_record/<str:trigger_task_record_id>', views.TriggerTaskRecordExecutionDetailsView.as_view(), name='task source trigger'),
+    path('workspace/<str:workspace_id>/trigger/<str:trigger_id>/trigger_task/<str:trigger_task_id>/trigger_task_record/<str:trigger_task_record_id>', views.TriggerTaskRecordExecutionDetailsView.as_view(), name='task source trigger'),
 ]
