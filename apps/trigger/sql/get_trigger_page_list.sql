2121dd0a19e0f944d926230036d93e0a4cedfc99
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2026-02-03 14:38:00 +0800

    fix: [Trigger] Add scheduled task next execution time

--- apps/trigger/sql/get_trigger_page_list.sql
+++ apps/trigger/sql/get_trigger_page_list.sql
@@ -1,4 +1,8 @@
-
+WITH scheduler AS (SELECT SPLIT_PART(id, ':', 2) as trigger_id,
+                          id,
+                          next_run_time
+                   FROM django_apscheduler_djangojob
+                   WHERE id LIKE 'trigger:%')
 SELECT *
 FROM (SELECT t.id,
              t.workspace_id,
@@ -13,6 +17,10 @@ FROM (SELECT t.id,
              t.user_id,
              (SELECT nick_name FROM "user" WHERE id = t.user_id) AS create_user,
              COALESCE(
+                     (ARRAY_AGG(sj.next_run_time ORDER BY sj.next_run_time))[1],
+                     NULL
+             )  as next_run_time,
+             COALESCE(
                      JSON_AGG(
                              JSON_BUILD_OBJECT(
                                      'type', tt.source_type,
@@ -23,6 +31,7 @@ FROM (SELECT t.id,
              )                                              AS trigger_task,
              STRING_AGG(COALESCE(app.name, tool.name), ' ') AS trigger_task_str
       FROM event_trigger t
+               LEFT JOIN scheduler sj ON sj.trigger_id=t.id::text
                LEFT JOIN event_trigger_task tt ON t.id = tt.trigger_id
                LEFT JOIN application app ON tt.source_type = 'APPLICATION' AND tt.source_id = app.id
                LEFT JOIN tool ON tt.source_type = 'TOOL' AND tt.source_id = tool.id
