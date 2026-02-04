2778c2436b6137d9e54ec4db1751df40f2a74733
shaohuzhang1 <shaohu.zhang@fit2cloud.com>
2026-02-04 11:38:32 +0800

    fix: [Interface Document] The upload file interface is missing parameters

--- apps/knowledge/api/file.py
+++ apps/knowledge/api/file.py
@@ -15,7 +15,17 @@ class FileUploadAPI(APIMixin):
                 'properties': {
                     'file': {
                         'type': 'string',
-                        'format': 'binary'  # Tells Swagger it's a file
+                        'format': 'binary',
+                        'description': '要上传的文件'
+
+                    },
+                    "source_id": {
+                        'type': 'string',
+                        'description': '资源id 如果source_type为[TEMPORARY_30_MINUTE,TEMPORARY_120_MINUTE,TEMPORARY_1_DAY,SYSTEM] 其他的需要为对应资源的id'
+                    },
+                    "source_type": {
+                        'type': 'string',
+                        'description': '资源类型[KNOWLEDGE,APPLICATION,TOOL,DOCUMENT,CHAT,SYSTEM,TEMPORARY_30_MINUTE,TEMPORARY_120_MINUTE,TEMPORARY_1_DAY]'
                     }
                 }
             }
