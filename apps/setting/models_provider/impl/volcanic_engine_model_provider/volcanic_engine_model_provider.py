359436b54cad366b4267abaed6e1ba530d0c734e
wxg0103 <727495428@qq.com>
2026-02-28 12:38:39 +0800

    fix: add default model info for additional model in volcanic engine provider

--- apps/setting/models_provider/impl/volcanic_engine_model_provider/volcanic_engine_model_provider.py
+++ apps/setting/models_provider/impl/volcanic_engine_model_provider/volcanic_engine_model_provider.py
@@ -98,6 +98,7 @@ model_info_manage = (
     .append_default_model_info(model_info_list[2])
     .append_default_model_info(model_info_list[3])
     .append_default_model_info(model_info_list[4])
+    .append_default_model_info(model_info_list[5])
     .append_model_info_list(model_info_embedding_list)
     .append_default_model_info(model_info_embedding_list[0])
     .build()
