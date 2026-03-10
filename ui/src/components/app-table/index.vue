8285ee1d23ba9dcc9e57cf3607d44624349b70eb
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2026-03-10 14:26:17 +0800

    fix: type

--- ui/src/components/app-table/index.vue
+++ ui/src/components/app-table/index.vue
@@ -86,7 +86,7 @@ const appTableRef = ref()
 const loading = ref(false)
 const showInput = ref(false)
 const inputValue = ref('')
-const tableHeight = ref<any>('')
+const tableHeight = ref<number | string>('')
 
 watch(showInput, (bool) => {
   if (!bool) {
