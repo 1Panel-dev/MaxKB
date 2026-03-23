fc5b6077f353e4c030858b4e79cf561a9d80a2d4
王良 <841369634@qq.com>
2026-03-23 17:02:55 +0800

    perf: system prompt double-generating

--- apps/application/flow/step_node/ai_chat_step_node/impl/base_chat_node.py
+++ apps/application/flow/step_node/ai_chat_step_node/impl/base_chat_node.py
@@ -377,7 +377,7 @@ class BaseChatNode(IChatNode):
 
     def generate_message_list(self, system: str, prompt: str, history_message):
         if system is not None and len(system) > 0:
-            return [SystemMessage(self.workflow_manage.generate_prompt(system)), *history_message,
+            return [SystemMessage(system), *history_message,
                     HumanMessage(self.workflow_manage.generate_prompt(prompt))]
         else:
             return [*history_message, HumanMessage(self.workflow_manage.generate_prompt(prompt))]
