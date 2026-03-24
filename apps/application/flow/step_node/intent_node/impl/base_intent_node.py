a39b3a5038ff44d921a2156fcb659c627a8a1e2c
王良 <841369634@qq.com>
2026-03-24 20:08:14 +0800

    小调整

--- apps/application/flow/step_node/intent_node/impl/base_intent_node.py
+++ apps/application/flow/step_node/intent_node/impl/base_intent_node.py
@@ -154,8 +154,8 @@ class BaseIntentNode(IIntentNode):
                 classification_id += 1
 
         # 构建输出JSON结构
-        output_reason = ', "reason": ""' if output_reason is True else ''
-        output_json = f'{{"classificationId": 0{output_reason}}}'
+        output_reason = ',\n"reason": ""' if output_reason is True else ''
+        output_json = f'{{\n"classificationId": 0{output_reason}\n}}'
 
         return (prompt_template or DEFAULT_PROMPT_TEMPLATE).format(
             classification_list=json.dumps(classification_list, ensure_ascii=False),
