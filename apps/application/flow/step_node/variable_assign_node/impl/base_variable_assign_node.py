e62183426abbb3e349ed656b97af8a8bb83489c4
王良 <841369634@qq.com>
2026-03-26 10:09:20 +0800

    版本回退避免报错。

--- apps/application/flow/step_node/variable_assign_node/impl/base_variable_assign_node.py
+++ apps/application/flow/step_node/variable_assign_node/impl/base_variable_assign_node.py
@@ -99,7 +99,7 @@ class BaseVariableAssignNode(IVariableAssignNode):
             evaluation(variable, val)
             result['output_value'] = val
         else:
-            reference = self.get_reference_content(variable['reference'])
+            reference = self.get_reference_content(variable.get('reference'))
             reference = self.convert(reference, variable.get('target_type'))
             evaluation(variable, reference)
             result['output_value'] = reference
@@ -109,7 +109,6 @@ class BaseVariableAssignNode(IVariableAssignNode):
         return result
 
     def execute(self, variable_list, **kwargs) -> NodeResult:
-        #
         result_list = []
         contains_chat_variable = False
         for variable in variable_list:
