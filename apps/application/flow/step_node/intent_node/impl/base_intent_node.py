fbbdb70bcc843df4ffbad760cc2b127bc58c58f6
王良 <841369634@qq.com>
2026-03-26 12:10:05 +0800

    撤回对正则的修正。

--- apps/application/flow/step_node/intent_node/impl/base_intent_node.py
+++ apps/application/flow/step_node/intent_node/impl/base_intent_node.py
@@ -131,7 +131,7 @@ class BaseIntentNode(IIntentNode):
 
         for message in history_message:
             if isinstance(message.content, str):
-                message.content = re.sub(r'<form_rander>[\d\D]*?<\/form_rander>', '', message.content)
+                message.content = re.sub('<form_rander>[\d\D]*?<\/form_rander>', '', message.content)
         return history_message
 
     def build_system_prompt(self) -> str:
--- apps/common/utils/common.py
+++ apps/common/utils/common.py
@@ -272,7 +272,7 @@ def post(post_function):
 
 
 def parse_md_image(content: str):
-    matches = re.finditer(r"!\[.*?\]\(.*?\)", content)
+    matches = re.finditer("!\[.*?\]\(.*?\)", content)
     image_list = [match.group() for match in matches]
     return image_list
 
@@ -330,7 +330,7 @@ def flat_map(array: List[List]):
 
 
 def parse_image(content: str):
-    matches = re.finditer(r"!\[.*?\]\(\.\/oss\/(image|file)\/.*?\)", content)
+    matches = re.finditer("!\[.*?\]\(\.\/oss\/(image|file)\/.*?\)", content)
     image_list = [match.group() for match in matches]
     return image_list
 
