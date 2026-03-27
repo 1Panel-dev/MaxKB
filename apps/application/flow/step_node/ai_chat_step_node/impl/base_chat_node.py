d4527dedf1f6190e3735630cea51b90eb31dce42
王良 <841369634@qq.com>
2026-03-27 15:31:34 +0800

    小调整

--- apps/application/flow/step_node/ai_chat_step_node/impl/base_chat_node.py
+++ apps/application/flow/step_node/ai_chat_step_node/impl/base_chat_node.py
@@ -453,7 +453,7 @@ class BaseChatNode(IChatNode):
                 tool_params[k] = self.workflow_manage.generate_prompt(tool_params[k])
             if type(v) == dict:
                 self.handle_variables(v)
-            if (type(v) == list) and (type(v[0]) == str):
+            if (type(v) == list) and len(v) > 0 and (type(v[0]) == str):
                 tool_params[k] = self.get_reference_content(v)
         return tool_params
 
