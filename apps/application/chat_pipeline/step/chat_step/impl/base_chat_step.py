8b94350315d137d91a58c65cba9c52265815b68c
王良 <841369634@qq.com>
2026-03-27 15:12:51 +0800

    小调整

--- apps/application/chat_pipeline/step/chat_step/impl/base_chat_step.py
+++ apps/application/chat_pipeline/step/chat_step/impl/base_chat_step.py
@@ -183,7 +183,7 @@ class BaseChatStep(IChatStep):
                 mcp_output_enable=True,
                 **kwargs):
         chat_model = get_model_instance_by_model_workspace_id(model_id, workspace_id,
-                                                              **model_params_setting) if model_id is not None else None
+                                                              **(model_params_setting or {})) if model_id is not None else None
         if stream:
             return self.execute_stream(message_list, chat_id, problem_text, post_response_handler, chat_model,
                                        paragraph_list,
