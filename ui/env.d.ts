793cd8002529556960ce8ce85bfe1fdd97e99856
shaohuzhang1 <80892890+shaohuzhang1@users.noreply.github.com>
2026-04-17 15:04:00 +0800

    fix: PDF preview of XSS attack (#5123)
    * fix: PDF preview of XSS attack
    
    * fix: PDF preview of XSS attack

--- ui/env.d.ts
+++ ui/env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 declare module 'katex'
+declare module 'pdfjs-dist/build/pdf.mjs'
 interface Window {
   sendMessage: ?((message: string, other_params_data: any) => void)
   chatUserProfile: ?(() => any)
