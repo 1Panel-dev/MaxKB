7cac2fa1878403d0a4b8aef5f7fa8b195ba604e5
wxg0103 <727495428@qq.com>
2026-04-21 16:18:11 +0800

    fix: enhance text_to_speech method to support additional models and handle audio data retrieval

--- apps/models_provider/impl/aliyun_bai_lian_model_provider/model/tts.py
+++ apps/models_provider/impl/aliyun_bai_lian_model_provider/model/tts.py
@@ -44,12 +44,46 @@ class AliyunBaiLianTextToSpeech(MaxKBBaseModel, BaseTextToSpeech):
         self.text_to_speech(_('Hello'))
 
     def text_to_speech(self, text):
+        global audio
         dashscope.api_key = self.api_key
         dashscope.base_http_api_url = self.base_url
         text = _remove_empty_lines(text)
         if 'sambert' in self.model:
             from dashscope.audio.tts import SpeechSynthesizer
             audio = SpeechSynthesizer.call(model=self.model, text=text, **self.params).get_audio_data()
+        elif 'qwen' in self.model:
+            response = dashscope.MultiModalConversation.call(
+                # 如需使用指令控制功能，请将model替换为qwen3-tts-instruct-flash
+                model=self.model,
+                api_key=self.api_key,
+                text=text,
+                **self.params
+            )
+            # 这个的接口返回格式和上面两个不太一样，直接返回了一个url地址，下载后就是音频文件
+            audio_url = response.output.audio.url
+            res = response.get(audio_url)
+            audio = res.content
+        elif 'MiniMax' in self.model:
+            import requests
+
+            api_url = f"{self.base_url}/services/aigc/multimodal-generation/generation"
+            headers = {
+                "Authorization": f"Bearer {self.api_key}",
+                "Content-Type": "application/json"
+            }
+            payload = {
+                "model": self.model,
+                "input": {
+                    "text": text,
+                    **self.params
+                }
+            }
+            response = requests.post(api_url, headers=headers, json=payload)
+            audio_hex = response.json().get("output", {}).get("data", {}).get("audio")
+            if audio_hex:
+                audio = bytes.fromhex(audio_hex)
+            else:
+                raise Exception('Failed to get audio data from response')
         else:
             from dashscope.audio.tts_v2 import SpeechSynthesizer
             synthesizer = SpeechSynthesizer(model=self.model, **self.params)
