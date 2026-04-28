3c22c411e9ec248e7a16d8e470e8147f1bde3af8
zhangzhanwei <zhanwei.zhang@fit2cloud.com>
2026-04-28 14:01:32 +0800

    fix: Search document node no relations

--- apps/application/flow/tools.py
+++ apps/application/flow/tools.py
@@ -815,7 +815,9 @@ target_source_node_mapping = {
               'parameter-extraction-node': lambda n: [n.get('properties').get('node_data').get('model_id')],
               'video-understand-node': lambda n: [n.get('properties').get('node_data').get('model_id')],
               },
-    'KNOWLEDGE': {'search-knowledge-node': lambda n: n.get('properties').get('node_data').get('knowledge_id_list')},
+    'KNOWLEDGE': {'search-knowledge-node': lambda n: n.get('properties').get('node_data').get('knowledge_id_list'),
+                  'search-document-node': lambda n: n.get('properties').get('node_data').get('knowledge_id_list')
+                  },
     'APPLICATION': {
         'application-node': lambda n: [n.get('properties').get('node_data').get('application_id')],
         'ai-chat-node': lambda n: [*(n.get('properties').get('node_data').get('application_ids') or [])],
