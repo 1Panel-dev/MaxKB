a5fb34b283e2dca5862bd4ee51ed1f74d0fdeedb
王良 <841369634@qq.com>
2026-04-29 21:02:00 +0800

    优化所有长度比较器，支持比较数字的长度

--- apps/application/flow/compare/len_le_compare.py
+++ apps/application/flow/compare/len_le_compare.py
@@ -12,7 +12,18 @@ from .compare import Compare
 class LenLECompare(Compare):
 
     def compare(self, source_value, compare, target_value):
+        # 获取target_value的长度
         try:
-            return len(source_value) <= int(target_value)
+            target_length = int(target_value)
         except Exception:
             return False
+
+        # 获取source_value的长度
+        try:
+            source_length = 0 if source_value is None else len(source_value)
+        except Exception:
+            # 可计算数字长度
+            source_length = len(str(source_value))
+
+        # 长度小于等于比较
+        return source_length <= target_length
