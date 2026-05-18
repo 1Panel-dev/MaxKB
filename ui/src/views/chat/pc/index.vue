cc5292554e7d4cb53faa371017619b046e8a2d3f
wxg0103 <727495428@qq.com>
2026-05-18 16:46:05 +0800

    fix: clean up formatting and remove unnecessary whitespace in index.vue

--- ui/src/views/chat/pc/index.vue
+++ ui/src/views/chat/pc/index.vue
@@ -43,16 +43,16 @@
                 chatUser.chat_profile.authentication_type === 'password'
               "
             >
-              <img src="@/assets/user-icon.svg" style="width: 54%" alt="" />
+              <img src="@/assets/user-icon.svg" style="width: 54%" alt=""/>
             </el-avatar>
             <el-dropdown v-else trigger="click" type="primary" class="w-full">
               <div class="flex align-center">
                 <el-avatar :size="32">
-                  <img src="@/assets/user-icon.svg" style="width: 54%" alt="" />
+                  <img src="@/assets/user-icon.svg" style="width: 54%" alt=""/>
                 </el-avatar>
                 <span v-show="!isPcCollapse" class="ml-8 color-text-primary">{{
-                  chatUser.chatUserProfile?.nick_name
-                }}</span>
+                    chatUser.chatUserProfile?.nick_name
+                  }}</span>
               </div>
 
               <template #dropdown>
@@ -60,7 +60,7 @@
                   <div class="flex align-center p-8">
                     <div class="mr-8 flex align-center">
                       <el-avatar :size="40">
-                        <img src="@/assets/user-icon.svg" style="width: 54%" alt="" />
+                        <img src="@/assets/user-icon.svg" style="width: 54%" alt=""/>
                       </el-avatar>
                     </div>
                     <div>
@@ -84,7 +84,7 @@
                     style="padding-top: 8px; padding-bottom: 8px"
                     @click="logout"
                   >
-                    <AppIcon iconName="app-export" class="color-secondary" />
+                    <AppIcon iconName="app-export" class="color-secondary"/>
                     {{ $t('layout.logout') }}
                   </el-dropdown-item>
                 </el-dropdown-menu>
@@ -99,7 +99,7 @@
           @click="isPcCollapse = !isPcCollapse"
         >
           <el-icon>
-            <component :is="isPcCollapse ? 'ArrowRightBold' : 'ArrowLeftBold'" />
+            <component :is="isPcCollapse ? 'ArrowRightBold' : 'ArrowLeftBold'"/>
           </el-icon>
         </el-button>
       </div>
@@ -148,13 +148,13 @@
                 <template #dropdown>
                   <el-dropdown-menu>
                     <el-dropdown-item @click="exportMarkdown"
-                      >{{ $t('common.export') }} Markdown</el-dropdown-item
+                    >{{ $t('common.export') }} Markdown</el-dropdown-item
                     >
                     <el-dropdown-item @click="exportHTML"
-                      >{{ $t('common.export') }} HTML</el-dropdown-item
+                    >{{ $t('common.export') }} HTML</el-dropdown-item
                     >
                     <el-dropdown-item @click="openPDFExport"
-                      >{{ $t('common.export') }} PDF</el-dropdown-item
+                    >{{ $t('common.export') }} PDF</el-dropdown-item
                     >
                   </el-dropdown-menu>
                 </template>
@@ -208,8 +208,8 @@
               </span> -->
               <span>
                 <el-button text @click="closeExecutionDetail">
-                  <el-icon size="20"><Close /></el-icon
-                ></el-button>
+                  <el-icon size="20"><Close/></el-icon
+                  ></el-button>
               </span>
             </div>
           </div>
@@ -225,7 +225,7 @@
                 :detail="executionDetail"
                 :appType="applicationDetail?.type"
               />
-              <ParagraphDocumentContent :detail="rightPanelDetail" v-else />
+              <ParagraphDocumentContent :detail="rightPanelDetail" v-else/>
             </el-scrollbar>
           </div>
         </div>
@@ -242,31 +242,36 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, nextTick, computed, watch, provide } from 'vue'
-import { marked } from 'marked'
-import { saveAs } from 'file-saver'
+import {ref, onMounted, nextTick, computed, watch, provide} from 'vue'
+import {marked} from 'marked'
+import {saveAs} from 'file-saver'
 import chatAPI from '@/api/chat/chat'
 import useStore from '@/stores'
 import useResize from '@/layout/hooks/useResize'
-import { hexToRgba } from '@/utils/theme'
-import { useRoute, useRouter } from 'vue-router'
+import {hexToRgba} from '@/utils/theme'
+import {useRoute, useRouter} from 'vue-router'
 import ResetPassword from '@/layout/layout-header/avatar/ResetPassword.vue'
-import { t } from '@/locales'
-import type { ResetCurrentUserPasswordRequest } from '@/api/type/user'
-import ExecutionDetailContent from '@/components/ai-chat/component/knowledge-source-component/ExecutionDetailContent.vue'
-import ParagraphSourceContent from '@/components/ai-chat/component/knowledge-source-component/ParagraphSourceContent.vue'
-import ParagraphDocumentContent from '@/components/ai-chat/component/knowledge-source-component/ParagraphDocumentContent.vue'
+import {t} from '@/locales'
+import type {ResetCurrentUserPasswordRequest} from '@/api/type/user'
+import ExecutionDetailContent
+  from '@/components/ai-chat/component/knowledge-source-component/ExecutionDetailContent.vue'
+import ParagraphSourceContent
+  from '@/components/ai-chat/component/knowledge-source-component/ParagraphSourceContent.vue'
+import ParagraphDocumentContent
+  from '@/components/ai-chat/component/knowledge-source-component/ParagraphDocumentContent.vue'
 import HistoryPanel from '@/views/chat/component/HistoryPanel.vue'
-import { cloneDeep } from 'lodash'
-import { getFileUrl } from '@/utils/common'
+import {cloneDeep} from 'lodash'
+import {getFileUrl} from '@/utils/common'
 import PdfExport from '@/components/pdf-export/index.vue'
+import JSEncrypt from "jsencrypt";
 
+const {user} = useStore()
 useResize()
 
 provide('scrollData', loadInfiniteScroll)
 provide('chatLogPagination', () => chatLogPagination)
 const pdfExportRef = ref<InstanceType<typeof PdfExport>>()
-const { common, chatUser } = useStore()
+const {common, chatUser} = useStore()
 const router = useRouter()
 const openPDFExport = () => {
   pdfExportRef.value?.open(document.getElementById('chatListId'))
@@ -301,8 +306,13 @@ const openResetPassword = () => {
 }
 
 const handleResetPassword = (param: ResetCurrentUserPasswordRequest) => {
-  chatAPI.resetCurrentPassword(param).then(() => {
-    router.push({ name: 'login' })
+  const JSEncryptCtor = (JSEncrypt as any)?.default ? (JSEncrypt as any).default : JSEncrypt;
+  const js = new (JSEncryptCtor as any)();
+  js.setPublicKey(user.rsaKey);
+  const jsonData = JSON.stringify(param);
+  const encryptedBase64 = js.encrypt(jsonData);
+  chatAPI.resetCurrentPassword({encryptedData: encryptedBase64}).then(() => {
+    router.push({name: 'login'})
   })
 }
 
@@ -329,7 +339,8 @@ const applicationDetail = computed({
   get: () => {
     return props.application_profile
   },
-  set: (v) => {},
+  set: (v) => {
+  },
 })
 
 const chatLogData = ref<any[]>([])
@@ -413,6 +424,7 @@ const chatLogPagination = ref({
   page_size: 20,
   current_page: 1,
 })
+
 function getChatLog(refresh?: boolean) {
   chatAPI
     .pageChat(chatLogPagination.value.current_page, chatLogPagination.value.page_size, left_loading)
@@ -509,7 +521,7 @@ async function exportMarkdown(): Promise<void> {
     })
     .join('\n')
 
-  const blob: Blob = new Blob([markdownContent], { type: 'text/markdown;charset=utf-8' })
+  const blob: Blob = new Blob([markdownContent], {type: 'text/markdown;charset=utf-8'})
   saveAs(blob, suggestedName)
 }
 
@@ -531,7 +543,7 @@ async function exportHTML(): Promise<void> {
     .join('\n')
   const htmlContent: any = marked(markdownContent)
 
-  const blob: Blob = new Blob([htmlContent], { type: 'text/html;charset=utf-8' })
+  const blob: Blob = new Blob([htmlContent], {type: 'text/html;charset=utf-8'})
   saveAs(blob, suggestedName)
 }
 
