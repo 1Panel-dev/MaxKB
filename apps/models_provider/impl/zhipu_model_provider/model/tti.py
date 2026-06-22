2b73c7a287a6abe595b500f3f496ecf5c395b43e
CaptainB <bin@fit2cloud.com>
2026-06-22 14:28:00 +0800

    fix: update ZhipuAI client import and upgrade langchain-mcp-adapters to version 0.3.0

--- apps/models_provider/impl/zhipu_model_provider/model/tti.py
+++ apps/models_provider/impl/zhipu_model_provider/model/tti.py
@@ -1,11 +1,11 @@
 from typing import Dict
 
+from common.config.tokenizer_manage_config import TokenizerManage
 from django.utils.translation import gettext
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from zhipuai import ZhipuAI
+from zai import ZhipuAiClient
 
-from common.config.tokenizer_manage_config import TokenizerManage
 from models_provider.base_model_provider import MaxKBBaseModel
 from models_provider.impl.base_tti import BaseTextToImage
 
@@ -23,10 +23,10 @@ class ZhiPuTextToImage(MaxKBBaseModel, BaseTextToImage):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.api_key = kwargs.get('api_key')
-        self.base_url = kwargs.get('base_url')
-        self.model = kwargs.get('model')
-        self.params = kwargs.get('params')
+        self.api_key = kwargs.get("api_key")
+        self.base_url = kwargs.get("base_url")
+        self.model = kwargs.get("model")
+        self.params = kwargs.get("params")
 
     @staticmethod
     def is_cache_model():
@@ -34,19 +34,16 @@ class ZhiPuTextToImage(MaxKBBaseModel, BaseTextToImage):
 
     @staticmethod
     def new_instance(model_type, model_name, model_credential: Dict[str, object], **model_kwargs):
-        optional_params = {'params': {'size': '1024x1024'}}
+        optional_params = {"params": {"size": "1024x1024"}}
         for key, value in model_kwargs.items():
-            if key not in ['model_id', 'use_local', 'streaming']:
-                optional_params['params'][key] = value
+            if key not in ["model_id", "use_local", "streaming"]:
+                optional_params["params"][key] = value
         return ZhiPuTextToImage(
             model=model_name,
-            api_key=model_credential.get('api_key'),
-            base_url=model_credential.get('base_url', "https://open.bigmodel.cn/api/paas/v4"),
+            api_key=model_credential.get("api_key"),
+            base_url=model_credential.get("base_url", "https://open.bigmodel.cn/api/paas/v4"),
             **optional_params,
         )
-
-    def is_cache_model(self):
-        return False
 
     def check_auth(self):
         chat = ChatOpenAI(
@@ -54,20 +51,16 @@ class ZhiPuTextToImage(MaxKBBaseModel, BaseTextToImage):
             base_url=self.base_url,
             model=self.model,
         )
-        chat.invoke([HumanMessage([{"type": "text", "text": gettext('Hello')}])])
+        chat.invoke([HumanMessage([{"type": "text", "text": gettext("Hello")}])])
 
         # self.generate_image('生成一个小猫图片')
 
     def generate_image(self, prompt: str, negative_prompt: str = None):
-        # chat = ChatZhipuAI(
-        #     zhipuai_api_key=self.api_key,
-        #     model_name=self.model,
-        # )
-        chat = ZhipuAI(api_key=self.api_key, base_url=self.base_url)
+        chat = ZhipuAiClient(api_key=self.api_key, base_url=self.base_url)
         response = chat.images.generations(
             model=self.model,  # 填写需要调用的模型编码
             prompt=prompt,  # 填写需要生成图片的文本
-            **self.params  # 填写额外参数
+            **self.params,  # 填写额外参数
         )
         file_urls = []
         try:
