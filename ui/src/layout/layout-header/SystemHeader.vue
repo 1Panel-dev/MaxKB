321d68d206f50445b8a46b9fa9bfb483ff09148b
lihao16 <lihao16@inspur.com>
2026-06-30 09:54:31 +0800

    fix(ui): restore 'Return to Workspace' button in SystemHeader
    The goHome button was accidentally removed along with TopAbout.
    Restored it with its original permission checks.

--- ui/src/layout/layout-header/SystemHeader.vue
+++ ui/src/layout/layout-header/SystemHeader.vue
@@ -7,12 +7,41 @@
 
     <div class="flex-between w-full align-center">
       <h4><el-divider class="ml-16 mr-16" direction="vertical" />{{ $t('views.system.title') }}</h4>
+      <div class="flex align-center mr-8">
+        <el-button
+          link
+          @click="goHome"
+          style="color: var(--text-primary)"
+          v-if="
+            hasPermission(
+              [
+                RoleConst.USER.getWorkspaceRole,
+                RoleConst.EXTENDS_USER.getWorkspaceRole,
+                RoleConst.EXTENDS_WORKSPACE_MANAGE.getWorkspaceRole,
+                RoleConst.WORKSPACE_MANAGE.getWorkspaceRole,
+              ],
+              'OR',
+            )
+          "
+        >
+          <AppIcon class="mr-8" iconName="app-workspace" style="font-size: 16px"></AppIcon>
+          {{ $t('views.workspace.toWorkspace') }}</el-button
+        >
+      </div>
     </div>
     <Avatar></Avatar>
   </div>
 </template>
 <script setup lang="ts">
+import { RoleConst } from '@/utils/permission/data'
 import Avatar from './avatar/index.vue'
+import { useRouter } from 'vue-router'
+import { hasPermission } from '@/utils/permission'
+
+const router = useRouter()
+const goHome = () => {
+  router.push('/')
+}
 </script>
 <style lang="scss" scoped>
 .app-top-bar-container {
