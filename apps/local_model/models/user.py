cd2cb40c1d84a2ef5cc18410248d447091e9286e
Binx <binx19980707@gmail.com>
2026-07-02 00:19:54 +0800

    fix: Remove uniqueness constraint from user nickname

--- apps/local_model/models/user.py
+++ apps/local_model/models/user.py
@@ -17,7 +17,7 @@ class User(models.Model):
     id = models.UUIDField(primary_key=True, max_length=128, default=uuid.uuid7, editable=False, verbose_name="主键id")
     email = models.EmailField(unique=True, null=True, blank=True, verbose_name="邮箱", db_index=True)
     phone = models.CharField(max_length=20, verbose_name="电话", default="", db_index=True)
-    nick_name = models.CharField(max_length=150, verbose_name="昵称", unique=True, db_index=True)
+    nick_name = models.CharField(max_length=150, verbose_name="昵称", db_index=True)
     username = models.CharField(max_length=150, unique=True, verbose_name="用户名", db_index=True)
     password = models.CharField(max_length=150, verbose_name="密码")
     role = models.CharField(max_length=150, verbose_name="角色")
