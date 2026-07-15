1c7dd7748c4acdab270bd571e945bd6a73ace51d
wxg0103 <727495428@qq.com>
2026-07-15 10:36:50 +0800

    fix: add exception handling for non-existent model in model parameter settings

--- apps/application/serializers/common.py
+++ apps/application/serializers/common.py
@@ -217,6 +217,8 @@ class ChatInfo:
         model_params_setting = None
         if model_id is not None:
             model = QuerySet(Model).filter(id=model_id).first()
+            if model is None:
+                raise Exception(_("Model does not exist"))
             credential = get_model_credential(model.provider, model.model_type, model.model_name)
             model_params_setting = credential.get_model_params_setting_form(model.model_name).get_default_form_data()
         return {
