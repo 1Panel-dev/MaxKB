c4d2976df3f596fb36ea4ec9dc3b9cff58bc39a1
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2026-08-19 19:28:58 +0800

    fix: delete folder

--- ui/src/mk-workflow/icons/utils.ts
+++
@@ -1,7 +0,0 @@
-import type { Component } from 'vue'
-
-const icons = import.meta.glob<{ default: Component }>('./**.vue', { eager: true })
-export function iconComponent(name: string) {
-  const url = `./${name}.vue`
-  return icons[url]?.default || null
-}
