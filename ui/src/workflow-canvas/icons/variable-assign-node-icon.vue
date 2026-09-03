1b8239f38e2c330040f2652201dab67177d0e619
panxinying <xinying.pan@fit2cloud.com>
2026-09-03 17:25:16 +0800

    feat: variable assign node

---
+++ ui/src/workflow-canvas/icons/variable-assign-node-icon.vue
@@ -0,0 +1,6 @@
+<template>
+  <el-avatar class="ai-avatar-gradient" shape="square">
+    <img src="@/assets/workflow/icon_ai_chat.svg" style="width: 75%" alt="" />
+  </el-avatar>
+</template>
+<script setup lang="ts"></script>
