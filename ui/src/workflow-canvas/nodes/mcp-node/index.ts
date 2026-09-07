2419fd891717020d5dfc0892f401f4458db354f3
panxinying <xinying.pan@fit2cloud.com>
2026-09-07 16:43:41 +0800

    feat: mcp node

---
+++ ui/src/workflow-canvas/nodes/mcp-node/index.ts
@@ -0,0 +1,11 @@
+import McpNodeVue from './index.vue'
+import { WorkflowNodeModel, WorkflowNodeView } from '@/workflow-canvas/core/workflow-node'
+import { WorkflowNodeType } from '@/workflow-canvas/types'
+
+class McpNodeView extends WorkflowNodeView {
+  constructor(props: ConstructorParameters<typeof WorkflowNodeView>[0]) {
+    super(props, McpNodeVue)
+  }
+}
+
+export default { type: WorkflowNodeType.McpNode, model: WorkflowNodeModel, view: McpNodeView }
