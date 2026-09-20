a12212bd42f20da3debcc6414d6cb63cfd1f516d
CaptainB <bin@fit2cloud.com>
2026-09-20 14:28:01 +0800

    chore: add DATA_UPLOAD_MAX_MEMORY_SIZE configuration for JSON import

--- apps/maxkb/settings/base/web.py
+++ apps/maxkb/settings/base/web.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 """
-    @project: MaxKB
-    @Author：虎虎
-    @file： web.py
-    @date：2025/11/5 14:53
-    @desc:
+@project: MaxKB
+@Author：虎虎
+@file： web.py
+@date：2025/11/5 14:53
+@desc:
 """
+
 import os
 from pathlib import Path
 
@@ -20,123 +21,124 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = CONFIG.get("SECRET_KEY") or 'django-insecure-zm^1_^i5)3gp^&0io6zg72&z!a*d=9kf9o2%uft+27l)+t(#3e'
+SECRET_KEY = CONFIG.get("SECRET_KEY") or "django-insecure-zm^1_^i5)3gp^&0io6zg72&z!a*d=9kf9o2%uft+27l)+t(#3e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = CONFIG.get_debug()
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.contenttypes',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'drf_spectacular',
-    'drf_spectacular_sidecar',
-    'users.apps.UsersConfig',
-    'tools.apps.ToolConfig',
-    'knowledge',
-    'common',
-    'system_manage',
-    'models_provider',
-    'application',
-    'chat',
-    'oss',
-    'trigger',
-    'django_apscheduler',
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_spectacular",
+    "drf_spectacular_sidecar",
+    "users.apps.UsersConfig",
+    "tools.apps.ToolConfig",
+    "knowledge",
+    "common",
+    "system_manage",
+    "models_provider",
+    "application",
+    "chat",
+    "oss",
+    "trigger",
+    "django_apscheduler",
     "portal",
-    "django.contrib.postgres"
+    "django.contrib.postgres",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'common.middleware.gzip.GZipMiddleware',
-    'common.middleware.chat_headers_middleware.ChatHeadersMiddleware',
-    'common.middleware.cross_domain_middleware.CrossDomainMiddleware',
-    'common.middleware.doc_headers_middleware.DocHeadersMiddleware',
-
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "common.middleware.gzip.GZipMiddleware",
+    "common.middleware.chat_headers_middleware.ChatHeadersMiddleware",
+    "common.middleware.cross_domain_middleware.CrossDomainMiddleware",
+    "common.middleware.doc_headers_middleware.DocHeadersMiddleware",
 ]
 
 REST_FRAMEWORK = {
-    'EXCEPTION_HANDLER': 'common.exception.handle_exception.handle_exception',
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_AUTHENTICATION_CLASSES': ['common.auth.authenticate.AnonymousAuthentication']
+    "EXCEPTION_HANDLER": "common.exception.handle_exception.handle_exception",
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": ["common.auth.authenticate.AnonymousAuthentication"],
 }
-STATICFILES_DIRS = [(os.path.join(PROJECT_DIR, 'ui', 'dist'))]
-STATIC_ROOT = os.path.join(BASE_DIR.parent, 'static')
-ROOT_URLCONF = 'maxkb.urls'
-APPS_DIR = os.path.join(PROJECT_DIR, 'apps')
+STATICFILES_DIRS = [(os.path.join(PROJECT_DIR, "ui", "dist"))]
+STATIC_ROOT = os.path.join(BASE_DIR.parent, "static")
+ROOT_URLCONF = "maxkb.urls"
+APPS_DIR = os.path.join(PROJECT_DIR, "apps")
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["apps/static/admin"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["apps/static/admin"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
-    {"NAME": "CHAT",
-     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-     'DIRS': ["apps/static/chat"],
-     'APP_DIRS': True,
-     'OPTIONS': {
-         'context_processors': [
-             'django.template.context_processors.debug',
-             'django.template.context_processors.request',
-             'django.contrib.auth.context_processors.auth',
-             'django.contrib.messages.context_processors.messages',
-         ],
-     },
-     },
-    {"NAME": "DOC",
-     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-     'DIRS': ["apps/static/drf_spectacular_sidecar"],
-     'APP_DIRS': True,
-     'OPTIONS': {
-         'context_processors': [
-             'django.template.context_processors.debug',
-             'django.template.context_processors.request',
-             'django.contrib.auth.context_processors.auth',
-             'django.contrib.messages.context_processors.messages',
-         ],
-     },
-     },
+    {
+        "NAME": "CHAT",
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["apps/static/chat"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+    {
+        "NAME": "DOC",
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["apps/static/drf_spectacular_sidecar"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
 ]
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'MaxKB API',
-    'DESCRIPTION': _('Intelligent customer service platform'),
-    'VERSION': 'v2',
-    'SERVE_INCLUDE_SCHEMA': False,
+    "TITLE": "MaxKB API",
+    "DESCRIPTION": _("Intelligent customer service platform"),
+    "VERSION": "v2",
+    "SERVE_INCLUDE_SCHEMA": False,
     # OTHER SETTINGS
-    'SWAGGER_UI_DIST': f'{CONFIG.get_admin_path()}/api-doc/swagger-ui-dist',  # shorthand to use the sidecar instead
-    'SWAGGER_UI_FAVICON_HREF': f'{CONFIG.get_admin_path()}/api-doc/swagger-ui-dist/favicon-32x32.png',
-    'REDOC_DIST': f'{CONFIG.get_admin_path()}/api-doc/redoc',
-    'SECURITY_DEFINITIONS': {
-        'Bearer': {
-            'type': 'apiKey',
-            'name': 'AUTHORIZATION',
-            'in': 'header',
+    "SWAGGER_UI_DIST": f"{CONFIG.get_admin_path()}/api-doc/swagger-ui-dist",  # shorthand to use the sidecar instead
+    "SWAGGER_UI_FAVICON_HREF": f"{CONFIG.get_admin_path()}/api-doc/swagger-ui-dist/favicon-32x32.png",
+    "REDOC_DIST": f"{CONFIG.get_admin_path()}/api-doc/redoc",
+    "SECURITY_DEFINITIONS": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "AUTHORIZATION",
+            "in": "header",
         }
-    }
+    },
 }
-WSGI_APPLICATION = 'maxkb.wsgi.application'
+WSGI_APPLICATION = "maxkb.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {'default': CONFIG.get_db_setting()}
+DATABASES = {"default": CONFIG.get_db_setting()}
 
 CACHES = CONFIG.get_cache_setting()
 
@@ -145,16 +147,16 @@ CACHES = CONFIG.get_cache_setting()
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -171,27 +173,27 @@ USE_TZ = True
 
 # 文件上传配置
 DATA_UPLOAD_MAX_NUMBER_FILES = 1000
+# 分段导入以 JSON 回传全文，默认允许 100 MiB，可通过 MAXKB_DATA_UPLOAD_MAX_MEMORY_SIZE（字节）调整。
+DATA_UPLOAD_MAX_MEMORY_SIZE = int(CONFIG.get("DATA_UPLOAD_MAX_MEMORY_SIZE", 100 * 1024 * 1024))
 
 # 支持的语言
 LANGUAGES = CONFIG.get_languages()
 # 翻译文件路径
-LOCALE_PATHS = [
-    os.path.join(BASE_DIR.parent, 'locales')
-]
+LOCALE_PATHS = [os.path.join(BASE_DIR.parent, "locales")]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-edition = 'CE'
+edition = "CE"
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-if os.environ.get('MAXKB_REDIS_SENTINEL_SENTINELS') is not None:
+if os.environ.get("MAXKB_REDIS_SENTINEL_SENTINELS") is not None:
     DJANGO_REDIS_CONNECTION_FACTORY = "django_redis.pool.SentinelConnectionFactory"
