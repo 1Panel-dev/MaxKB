6bcfc540f736aca269cab8b71b1c82e38d9bc36f
panxinying <xinying.pan@fit2cloud.com>
2026-09-20 11:53:25 +0800

    fix: Permission control for backend system menus

--- apps/common/auth/constants/permission_constants.py
+++ apps/common/auth/constants/permission_constants.py
@@ -1082,7 +1082,7 @@ class PermissionConstants(Enum):
             group=Group.RESOURCE_PERMISSION,
             sub_group=Group.KNOWLEDGE_RESOURCE_PERMISSION,
             operate=Operate.READ,
-            bit_index=0,
+            bit_index=2,
         ),
         PermissionMeta(
             role_list=[RoleConstants.ADMIN, RoleConstants.WORKSPACE_MANAGE],
@@ -1096,7 +1096,7 @@ class PermissionConstants(Enum):
             group=Group.RESOURCE_PERMISSION,
             sub_group=Group.KNOWLEDGE_RESOURCE_PERMISSION,
             operate=Operate.EDIT,
-            bit_index=1,
+            bit_index=3,
         ),
         PermissionMeta(
             role_list=[RoleConstants.ADMIN, RoleConstants.WORKSPACE_MANAGE],
@@ -1107,7 +1107,7 @@ class PermissionConstants(Enum):
 
     TOOL_RESOURCE_PERMISSION_READ = (
         Permission(
-            group=Group.RESOURCE_PERMISSION, sub_group=Group.TOOL_RESOURCE_PERMISSION, operate=Operate.READ, bit_index=0
+            group=Group.RESOURCE_PERMISSION, sub_group=Group.TOOL_RESOURCE_PERMISSION, operate=Operate.READ, bit_index=4
         ),
         PermissionMeta(
             role_list=[RoleConstants.ADMIN, RoleConstants.WORKSPACE_MANAGE],
@@ -1121,7 +1121,7 @@ class PermissionConstants(Enum):
             group=Group.RESOURCE_PERMISSION,
             sub_group=Group.TOOL_RESOURCE_PERMISSION,
             operate=Operate.EDIT,
-            bit_index=1,
+            bit_index=5,
         ),
         PermissionMeta(
             role_list=[RoleConstants.ADMIN, RoleConstants.WORKSPACE_MANAGE],
@@ -1135,7 +1135,7 @@ class PermissionConstants(Enum):
             group=Group.RESOURCE_PERMISSION,
             sub_group=Group.MODEL_RESOURCE_PERMISSION,
             operate=Operate.READ,
-            bit_index=0,
+            bit_index=6,
         ),
         PermissionMeta(
             role_list=[RoleConstants.ADMIN, RoleConstants.WORKSPACE_MANAGE],
@@ -1149,7 +1149,7 @@ class PermissionConstants(Enum):
             group=Group.RESOURCE_PERMISSION,
             sub_group=Group.MODEL_RESOURCE_PERMISSION,
             operate=Operate.EDIT,
-            bit_index=1,
+            bit_index=7,
         ),
         PermissionMeta(
             role_list=[RoleConstants.ADMIN, RoleConstants.WORKSPACE_MANAGE],
@@ -1867,17 +1867,17 @@ class PermissionConstants(Enum):
             scope=[PermissionScopeConstants.SYSTEM],
         ),
     )
-
+    # ==================== 对话用户认证 ====================
     CHAT_USER_AUTH_READ = (
-        Permission(group=Group.CHAT_USER_GROUP, sub_group=Group.CHAT_USER_GROUP, operate=Operate.READ, bit_index=6),
+        Permission(group=Group.CHAT_USER_AUTH, sub_group=Group.CHAT_USER_AUTH, operate=Operate.READ, bit_index=0),
         PermissionMeta(
             role_list=[RoleConstants.ADMIN], category=Category.CHAT_CLIENT, scope=[PermissionScopeConstants.SYSTEM]
         ),
     )
 
-    # ==================== 对话用户认证 ====================
+
     CHAT_USER_AUTH_EDIT = (
-        Permission(group=Group.CHAT_USER_AUTH, sub_group=Group.CHAT_USER_AUTH, operate=Operate.EDIT, bit_index=0),
+        Permission(group=Group.CHAT_USER_AUTH, sub_group=Group.CHAT_USER_AUTH, operate=Operate.EDIT, bit_index=1),
         PermissionMeta(
             role_list=[RoleConstants.ADMIN], category=Category.CHAT_CLIENT, scope=[PermissionScopeConstants.SYSTEM]
         ),
