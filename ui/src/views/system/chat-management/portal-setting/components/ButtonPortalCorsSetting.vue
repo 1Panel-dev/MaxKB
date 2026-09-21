fe05fe4c53458c9aa6d54aa6600153ccc3c09833
wangdan-fit2cloud <dan.wang@fit2cloud.com>
2026-09-21 18:59:16 +0800

    feat: portal

--- ui/src/views/system/chat-management/portal-setting/components/ButtonPortalCorsSetting.vue
+++ ui/src/views/system/chat-management/portal-setting/components/ButtonPortalCorsSetting.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const props = defineProps<{ saving?: boolean; save: (origins: string[]) => Promise<void> }>()
+const props = defineProps<{ disabled: boolean; saving?: boolean; save: (origins: string[]) => Promise<void> }>()
 const allowedOrigins = defineModel<string[]>({ default: () => [] })
 
 /* 跨域地址草稿，取消不修改已保存配置。 */
@@ -11,6 +11,8 @@ const corsAddressPlaceholder =
   '请输入允许的跨域地址，开启后不输入跨域地址则不限制。\n跨域地址一行一个，如：\nhttp://127.0.0.1:5678\nhttps://dataease.io'
 
 function handleOpenCorsSetting() {
+  if (props.disabled || props.saving) return
+
   corsAddressDraft.value = allowedOrigins.value.join('\n')
   corsVisible.value = true
 }
@@ -25,7 +27,7 @@ function parseCorsOrigins(value: string) {
 }
 
 function handleSaveCorsSetting() {
-  if (props.saving) return
+  if (props.disabled || props.saving) return
   return props.save(parseCorsOrigins(corsAddressDraft.value)).then(() => {
     corsVisible.value = false
   })
@@ -38,7 +40,7 @@ function handleClosed() {
 
 <template>
   <!-- 配置跨域地址 -->
-  <el-button text type="primary" title="跨域设置" @click="handleOpenCorsSetting">
+  <el-button text type="primary" title="跨域设置" :disabled="disabled || saving" @click="handleOpenCorsSetting">
     <MkIcon name="icon_setting" />
   </el-button>
   <MkDialog v-model="corsVisible" title="跨域设置" @closed="handleClosed">
